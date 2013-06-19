0f7e2c5763938c4bb8cc262056d77fb0f2f226fc
Cosimo Cecchi <cosimo@endlessm.com>
2013-06-19 13:28:15 -0700

    Make clicks on empty areas work with multiple monitors
    Add an isPrimary argument to Main.overview.addAction() that determines
    whether the action only applies to the primary monitor.
    At the same time, add an additional actor that spans the whole overlay
    group to catch events behind overview elements on all monitors.
    
    [endlessm/eos-shell#332]

--- js/ui/overview.js
+++ js/ui/overview.js
@@ -162,13 +162,22 @@ const Overview = new Lang.Class({
 
         this._overviewCreated = true;
 
+        // this._allMonitorsGroup is a simple actor that covers all monitors,
+        // used to install actions that apply to all monitors
+        this._allMonitorsGroup = new Clutter.Actor({ reactive: true });
+        this._allMonitorsGroup.add_constraint(
+            new Clutter.BindConstraint({ source: Main.layoutManager.overviewGroup,
+                                         coordinate: Clutter.BindCoordinate.ALL }));
+
         /* Translators: This is the main view to select
            activities. See also note for "Activities" string. */
         this._overview = new St.BoxLayout({ name: 'overview',
                                             accessible_name: _("Overview"),
+                                            reactive: true,
                                             vertical: true });
         this._overview.add_constraint(new Monitor.MonitorConstraint({ primary: true }));
         this._overview._delegate = this;
+        this._allMonitorsGroup.add_actor(this._overview);
 
         // The main Background actors are inside global.window_group which are
         // hidden when displaying the overview, so we create a new
@@ -200,7 +209,7 @@ const Overview = new Lang.Class({
         Main.layoutManager.overviewGroup.add_child(this._coverPane);
         this._coverPane.connect('event', Lang.bind(this, function (actor, event) { return Clutter.EVENT_STOP; }));
 
-        Main.layoutManager.overviewGroup.add_child(this._overview);
+        Main.layoutManager.overviewGroup.add_child(this._allMonitorsGroup);
 
         this._coverPane.hide();
 
@@ -387,11 +396,14 @@ const Overview = new Lang.Class({
         return Clutter.EVENT_PROPAGATE;
     },
 
-    addAction: function(action) {
+    addAction: function(action, isPrimary) {
         if (this.isDummy)
             return;
 
-        this._backgroundGroup.add_action(action);
+        if (isPrimary)
+            this._overview.add_action(action);
+        else
+            this._allMonitorsGroup.add_action(action);
     },
 
     _getDesktopClone: function() {
