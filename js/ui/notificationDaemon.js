89669881d263ec020fd0a0cbab8a18f63d4cd93a
Cosimo Cecchi <cosimo@endlessm.com>
2016-11-18 17:21:47 -0800

    notificationDaemon: add method to get app for GtkNotification source
    https://phabricator.endlessm.com/T13823-gtk-notification-daemon

--- js/ui/notificationDaemon.js
+++ js/ui/notificationDaemon.js
@@ -763,6 +763,10 @@ const GtkNotificationDaemonAppSource = new Lang.Class({
         }
         return [this._appId, notifications];
     },
+
+    get app() {
+        return this._app;
+    }
 });
 
 const GtkNotificationsIface = '<node> \
