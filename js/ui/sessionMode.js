fc96603cd9628e9daa099c9cd097531f61421ab0
Mario Sanchez Prada <mario@endlessm.com>
2017-05-06 01:32:34 +0100

    bottomPanel: Reorganize elements and style in the bottom panel
      - Removed Activities and AppMenu buttoms from the panel.
      - Moved the clock button to the right of the panel and adjust style.
      - Make sure that the clock button is only shown in the 'user' session mode.
      - Adapt style of boxpointers and popup menus.
    
    https://phabricator.endlessm.com/T17880

--- js/ui/sessionMode.js
+++ js/ui/sessionMode.js
@@ -49,7 +49,7 @@ const _modes = {
         components: ['polkitAgent'],
         panel: {
             left: [],
-            center: ['dateMenu'],
+            center: [],
             right: ['a11yGreeter', 'keyboard', 'aggregateMenu'],
         },
         panelStyle: 'login-screen'
@@ -100,9 +100,9 @@ const _modes = {
                      'keyring', 'autorunManager', 'automountManager'],
 
         panel: {
-            left: ['activities', 'appMenu'],
-            center: ['dateMenu'],
-            right: ['a11y', 'keyboard', 'aggregateMenu']
+            left: [],
+            center: [],
+            right: ['a11y', 'keyboard', 'aggregateMenu', 'dateMenu']
         }
     }
 };
