b88acf6d5bc40fe57afc1e3df080e8bd0317e964
Mario Sanchez Prada <mario@endlessm.com>
2017-05-19 22:52:11 +0100

    Add isAnimating property and 'search-progress-updated' signal to SearchResults
    This will be used from a new class ViewDisplay that will be used to manage
    different widgets that will be used to compose the desktop, including the
    search box that will be placed on top of the icons grid.

--- js/ui/search.js
+++ js/ui/search.js
@@ -411,6 +411,7 @@ const SearchResults = new Lang.Class({
 
         this._terms = [];
         this._results = {};
+        this._isAnimating = false;
 
         this._providers = [];
 
@@ -603,25 +604,50 @@ const SearchResults = new Lang.Class({
         });
     },
 
+    get isAnimating() {
+        return this._isAnimating;
+    },
+
+    set isAnimating (v) {
+        if (this._isAnimating == v)
+            return;
+
+        this._isAnimating = v;
+        this._updateSearchProgress();
+        if (!this._isAnimating) {
+            this._providers.forEach(Lang.bind(this, function (provider) {
+                let results = this._results[provider.id];
+                if (results)
+                    this._updateResults(provider, results);
+            }));
+        }
+    },
+
     _updateSearchProgress: function () {
         let haveResults = this._providers.some(function(provider) {
             let display = provider.display;
             return (display.getFirstResult() != null);
         });
+        let showStatus = !haveResults && !this.isAnimating;
 
         this._scrollView.visible = haveResults;
-        this._statusBin.visible = !haveResults;
+        this._statusBin.visible = showStatus;
 
-        if (!haveResults) {
+        if (showStatus) {
             if (this.searchInProgress) {
                 this._statusText.set_text(_("Searching…"));
             } else {
                 this._statusText.set_text(_("No results."));
             }
         }
+
+        this.emit('search-progress-updated');
     },
 
     _updateResults: function(provider, results) {
+        if (this.isAnimating)
+            return;
+
         let terms = this._terms;
         let display = provider.display;
 
@@ -634,6 +660,10 @@ const SearchResults = new Lang.Class({
     },
 
     activateDefault: function() {
+        // If we are about to activate a result, we are done animating and need
+        // to update the display immediately.
+        this.isAnimating = false;
+
         // If we have a search queued up, force the search now.
         if (this._searchTimeoutId > 0)
             this._doSearch();
@@ -673,6 +703,7 @@ const SearchResults = new Lang.Class({
         }
     }
 });
+Signals.addSignalMethods(SearchResults.prototype);
 
 const ProviderIcon = new Lang.Class({
     Name: 'ProviderIcon',
