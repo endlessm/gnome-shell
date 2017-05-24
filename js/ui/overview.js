edff9abab3455a3d09d0afcc32c35b7564a180c6
Mario Sanchez Prada <mario@endlessm.com>
2017-05-24 13:57:07 +0100

    Remove Vignette effect from the overview
    On Endless we are most of the time in the Overview (we start up into it!),
    so we don't want the Vignette effect from upstream GNOME Shell, that would
    obscure the whole desktop otherwise.

--- js/ui/overview.js
+++ js/ui/overview.js
@@ -179,8 +179,7 @@ const Overview = new Lang.Class({
 
         for (let i = 0; i < Main.layoutManager.monitors.length; i++) {
             let bgManager = new Background.BackgroundManager({ container: this._backgroundGroup,
-                                                               monitorIndex: i,
-                                                               vignette: true });
+                                                               monitorIndex: i });
             this._bgManagers.push(bgManager);
         }
     },
@@ -190,7 +189,6 @@ const Overview = new Lang.Class({
         for (let i = 0; i < backgrounds.length; i++) {
             Tweener.addTween(backgrounds[i],
                              { brightness: 1.0,
-                               vignette_sharpness: 0.0,
                                time: SHADE_ANIMATION_TIME,
                                transition: 'easeOutQuad'
                              });
@@ -202,7 +200,6 @@ const Overview = new Lang.Class({
         for (let i = 0; i < backgrounds.length; i++) {
             Tweener.addTween(backgrounds[i],
                              { brightness: Lightbox.VIGNETTE_BRIGHTNESS,
-                               vignette_sharpness: Lightbox.VIGNETTE_SHARPNESS,
                                time: SHADE_ANIMATION_TIME,
                                transition: 'easeOutQuad'
                              });
@@ -580,8 +577,6 @@ const Overview = new Lang.Class({
                            onComplete: this._showDone,
                            onCompleteScope: this
                          });
-        this._shadeBackgrounds();
-
         this._coverPane.raise_top();
         this._coverPane.show();
         this.emit('showing');
