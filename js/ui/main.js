656f789b1d2cde8ace8d830ea78ba4d2540b2323
Mario Sanchez Prada <mario@endlessm.com>
2017-05-27 04:38:26 +0100

    workspaceMonitor: Add a monitor to track when there are visible windows on the desktop.
    This will be useful to implement the "Endless button" in the bottom panel (to
    decide when we need to show the apps view or blink the search entry box) and
    for any other actions that depend on not having any window opened in the desktop.
    
    Starting in EOS 3.4 / GNOME Shell 3.26, this also fixes a corner case that has not
    been contemplated before, because it normally happens when manipulating the status
    icon in the tray area, which was fairly hidden in a sliding panel until now: when
    you have an app hidden (not minimized) because of interacting with it via the tray
    area icon (e.g. Telegram) and clicking on its icon again should make it visible again,
    the previous code prevented that from happening unless some other windows wass already
    visible, because nothing would hide the overview in this scenario, keeping the app hidden.
    
    https://phabricator.endlessm.com/T17937

--- js/ui/main.js
+++ js/ui/main.js
@@ -46,6 +46,7 @@ const Magnifier = imports.ui.magnifier;
 const XdndHandler = imports.ui.xdndHandler;
 const Util = imports.misc.util;
 const Watermark = imports.ui.watermark;
+const WorkspaceMonitor = imports.ui.workspaceMonitor;
 
 const A11Y_SCHEMA = 'org.gnome.desktop.a11y.keyboard';
 const STICKY_KEYS_ENABLE = 'stickykeys-enable';
@@ -82,6 +83,7 @@ var xdndHandler = null;
 var keyboard = null;
 var layoutManager = null;
 var trayArea = null;
+var workspaceMonitor = null;
 let _startDate;
 let _defaultCssStylesheet = null;
 let _cssStylesheet = null;
@@ -186,6 +188,9 @@ function _initializeUI() {
 
     layoutManager.init();
     overview.init();
+
+    // WorkspaceMonitor expects layoutManager to be ready, initialize it here.
+    workspaceMonitor = new WorkspaceMonitor.WorkspaceMonitor();
 
     _a11ySettings = new Gio.Settings({ schema_id: A11Y_SCHEMA });
 
