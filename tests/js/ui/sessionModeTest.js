ff71451f888d27e37a0cb017cc006c98dc520c3a
Sam Spilsbury <sam@endlessm.com>
2017-05-30 08:27:18 +0800

    sessionMode: Add test for clock position

---
+++ tests/js/ui/sessionModeTest.js
@@ -0,0 +1,34 @@
+const GLib = imports.gi.GLib;
+
+function resetEnvironment() {
+    let settings = jasmine.createSpyObj('settings', [
+        'connect',
+        'get_boolean',
+        'get_value',
+        'reset',
+    ]);
+    settings.get_value.and.returnValue(new GLib.Variant('a{sv}',
+        [new GLib.Variant('{sv}', ['desktop', new GLib.Variant('as', [])])]));
+    settings.get_boolean.and.returnValue(true);
+
+    window.global = {
+        settings: settings,
+    };
+    window._ = (str) => str;
+    window.C_ = (ctx, str) => str;
+}
+resetEnvironment();  // Needed for following import
+
+let sessionModeImport = imports.js.ui.sessionMode;
+
+describe('The clock', function () {
+    beforeEach(resetEnvironment);
+
+    it('is on the right before the hotcorner', function () {
+        let modes = sessionModeImport._modes;
+
+        let rightPanel = modes['user']['panel']['right'];
+
+        expect(rightPanel[rightPanel.length - 2]).toEqual('dateMenu');
+    });
+});
