1d8d339df229d57ea71647b1c1d22f0bf0227ef0
Cosimo Cecchi <cosimoc@gnome.org>
2017-06-03 15:37:36 -0700

    sessionMode: add a property to disable window manager menus
    We will use this to disable menus in the initial-setup session.

--- js/ui/windowMenu.js
+++ js/ui/windowMenu.js
@@ -199,6 +199,9 @@ var WindowMenuManager = new Lang.Class({
     },
 
     showWindowMenuForWindow: function(window, type, rect) {
+	if (!Main.sessionMode.hasWmMenus)
+	    return;
+
         let menuType = (type == Meta.WindowMenuType.WM) ? WindowMenu : AppMenu;
         let menu = new menuType(window, this._sourceActor);
 
