37302a186db7b04e25edf2637ef366a3bcaf1849
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2017-06-07 18:20:47 -0300

    endlessButton: Add a tooltip
    Since we're modifying the behavior of the bottom
    left button, this commit adds a tooltip to explain
    the new functionality.
    
    https://phabricator.endlessm.com/T17435

--- js/ui/endlessButton.js
+++ js/ui/endlessButton.js
@@ -15,12 +15,27 @@ const EndlessButton = new Lang.Class({
     _init: function() {
         this.parent(_('Endless Button'));
         this.actor.add_style_class_name('endless-button');
+        this.actor.connect('notify::hover', Lang.bind(this, this._onHoverChanged));
 
         let iconFile;
         iconFile = Gio.File.new_for_uri('resource:///org/gnome/shell/theme/endless-button-symbolic.svg');
         this._gicon = new Gio.FileIcon({ file: iconFile });
 
         this.setIcon(this._gicon);
+
+        this._setupTooltipText();
+    },
+
+    _setupTooltipText: function() {
+
+        // Create a new tooltip label
+        this._label = new St.Label({ text: _("Show Desktop"),
+                                     style_class: 'app-icon-hover-label' });
+
+        this._labelOffsetY = 0;
+        this._label.connect('style-changed', Lang.bind(this, function(actor, forHeight, alloc) {
+            this._labelOffsetY = this._label.get_theme_node().get_length('-label-offset-y');
+        }));
     },
 
     // overrides default implementation from PanelMenu.Button
@@ -33,5 +48,30 @@ const EndlessButton = new Lang.Class({
         }
 
         return Clutter.EVENT_PROPAGATE;
+    },
+
+    _onHoverChanged: function() {
+        if (!this._label)
+            return;
+
+        if (this.actor.hover) {
+            if (this._label.get_parent())
+                return;
+
+            Main.uiGroup.add_actor(this._label);
+            this._label.raise_top();
+
+            // Update the tooltip position
+            let iconMidpoint = this.actor.get_transformed_position()[0] + this.actor.width / 2;
+            this._label.translation_x = Math.floor(iconMidpoint - this._label.width / 2);
+            this._label.translation_y = Math.floor(this.actor.get_transformed_position()[1] - this._labelOffsetY);
+
+            // Clip left edge to be the left edge of the screen
+            this._label.translation_x = Math.max(this._label.translation_x, 0);
+        } else {
+            // Remove the tooltip from uiGroup
+            if (this._label.get_parent() != null)
+                Main.uiGroup.remove_actor(this._label);
+        }
     }
 });
