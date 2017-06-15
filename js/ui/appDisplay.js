4b383a6f2a501cd7524b7a2d58519164e9af460d
Mario Sanchez Prada <mario@endlessm.com>
2017-06-15 15:34:18 +0100

    Don't animate the icons when entering/leaving the icon grid

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -210,8 +210,11 @@ const BaseAppView = new Lang.Class({
 
     _doSpringAnimation: function(animationDirection) {
         this._grid.actor.opacity = 255;
-        this._grid.animateSpring(animationDirection,
-                                 Main.overview.getShowAppsButton());
+
+        // We don't do the icon grid animations on Endless, but we still need
+        // to call this method so that the animation-done signal gets emitted,
+        // in order not to break the transitoins.
+        this._grid.animateSpring(animationDirection, null);
     },
 
     animate: function(animationDirection, onComplete) {
