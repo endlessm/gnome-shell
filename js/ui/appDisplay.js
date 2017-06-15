149019294990e947fff69e7d17bc06b3fb1cb11e
Mario Sanchez Prada <mario@endlessm.com>
2017-06-15 14:37:57 +0100

    Allow opening the background menu popup from the APPS view
    This allows opening the menu from the icon grid view, as long
    as no folder is open (which disables the click action).

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -14,6 +14,7 @@ const Mainloop = imports.mainloop;
 const Atk = imports.gi.Atk;
 
 const AppFavorites = imports.ui.appFavorites;
+const BackgroundMenu = imports.ui.backgroundMenu;
 const BoxPointer = imports.ui.boxpointer;
 const DND = imports.ui.dnd;
 const GrabHelper = imports.ui.grabHelper;
@@ -442,7 +443,16 @@ const AllView = new Lang.Class({
             if (!this._currentPopup.actor.contains(actor))
                 this._currentPopup.popdown();
         }));
-        this._eventBlocker.add_action(this._clickAction);
+        Main.overview.addAction(this._clickAction, false);
+        this._eventBlocker.bind_property('reactive', this._clickAction, 'enabled', GObject.BindingFlags.SYNC_CREATE);
+
+        this._bgAction = new Clutter.ClickAction();
+        Main.overview.addAction(this._bgAction, true);
+        BackgroundMenu.addBackgroundMenuForAction(this._bgAction, Main.layoutManager);
+        this._clickAction.bind_property('enabled', this._bgAction, 'enabled',
+                                        GObject.BindingFlags.SYNC_CREATE | GObject.BindingFlags.INVERT_BOOLEAN);
+        this.actor.bind_property('mapped', this._bgAction, 'enabled',
+                                 GObject.BindingFlags.SYNC_CREATE);
 
         this._displayingPopup = false;
 
