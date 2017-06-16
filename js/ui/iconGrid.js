afc22cab9602553ecc9458bff9a46d1b6046e647
Mario Sanchez Prada <mario@endlessm.com>
2017-06-16 23:36:32 +0100

    iconGrid: Adapt calculation of space between icons and pages
    Again, the upstream shell calculates the amount of space between icons
    dynamically depending on the available size of the screen, but we want
    to have a fixed size grid, so let's remove that code to allow
    enforcing our preference from the CSS file.
    
    https://phabricator.endlessm.com/T17658

--- js/ui/iconGrid.js
+++ js/ui/iconGrid.js
@@ -708,7 +708,6 @@ const IconGrid = new Lang.Class({
     adaptToSize: function(availWidth, availHeight) {
         this._fixedHItemSize = this._hItemSize;
         this._fixedVItemSize = this._vItemSize;
-        this._updateSpacingForSize(availWidth, availHeight);
     }
 });
 Signals.addSignalMethods(IconGrid.prototype);
