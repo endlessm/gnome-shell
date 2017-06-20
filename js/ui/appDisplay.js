203146a3269109031b4829a6500cb6e5cb568780
Mario Sanchez Prada <mario@endlessm.com>
2017-06-20 15:48:08 +0100

    iconGrid: Don't consider unused vertical space for preferred height
    Upstream always considers all the space to report the preferred
    height, even if there are no enough icons to fill it. This causes
    problems with our desired layout where we want to pack the icon
    grid centered under the desktop search actor, since the grid will
    try to take up all the possible allocation, pushing the search
    entry to the top of the desktop.
    
    This fixes this situation by not requesting more height than what
    is actually needed. This means that icons will be pushed outside
    of the allocated space for the grid when opening a folder, which
    is not ideal, but probably good enough for now (and something that
    can be fixed as a separate ticket). To prevent some too extreme
    situations, though, we set the minimum amount of rows to 2, so that
    we always leave space for folders in case we only have one row.
    
    https://phabricator.endlessm.com/T17658

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -28,7 +28,7 @@ var MAX_APPLICATION_WORK_MILLIS = 75;
 var MENU_POPUP_TIMEOUT = 600;
 var MAX_COLUMNS = 7;
 var MIN_COLUMNS = 4;
-var MIN_ROWS = 4;
+var MIN_ROWS = 1;
 
 var INACTIVE_GRID_OPACITY = 77;
 // This time needs to be less than IconGrid.EXTRA_SPACE_ANIMATION_TIME
@@ -65,6 +65,8 @@ const SwitcherooProxy = Gio.DBusProxy.makeProxyWrapper(SwitcherooProxyInterface)
 let discreteGpuAvailable = false;
 
 // Endless-specific definitions below this point
+
+const EOS_DESKTOP_MIN_ROWS = 2;
 
 const EOS_LINK_PREFIX = 'eos-link-';
 
@@ -300,7 +302,8 @@ class AllViewContainer extends St.Widget {
 
 var AllView = class AllView extends BaseAppView {
     constructor() {
-        super({ usePagination: true }, null);
+        super({ usePagination: true },
+              { minRows: EOS_DESKTOP_MIN_ROWS });
         this.actor = new AllViewContainer(this._grid.actor);
         this._scrollView = this.actor.scrollView;
         this._stack = this.actor.stack;
