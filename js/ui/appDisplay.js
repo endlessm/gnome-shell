97e680e4f3ce57a978bdd34a682fc2dd6fdea2c5
Roddy Shuler <roddy@endlessm.com>
2017-06-20 19:09:09 -0700

    appDisplay: use upstream folder icon creation
    Instead of providing custom folder icons, we are going to use
    the mosaic of icons from the first four apps in the folder.
    Previously, users had trouble realizing that our old icons
    represented folders of apps, and this design is much clearer.
    It also has the benefit of being closer to upstream.
    
    This partially reverts 02c171c7dc278816eda30fb8375ac7c731855418.
    
    https://phabricator.endlessm.com/T17785

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1097,8 +1097,26 @@ const FolderView = new Lang.Class({
     },
 
     createFolderIcon: function(size) {
-        let icon = this._dirInfo.get_icon();
-        return new St.Icon({ gicon: icon });
+        let layout = new Clutter.GridLayout();
+        let icon = new St.Widget({ layout_manager: layout,
+                                   style_class: 'app-folder-icon' });
+        layout.hookup_style(icon);
+        let subSize = Math.floor(FOLDER_SUBICON_FRACTION * size);
+
+        let numItems = this._allItems.length;
+        let rtl = icon.get_text_direction() == Clutter.TextDirection.RTL;
+        for (let i = 0; i < 4; i++) {
+            let bin;
+            if (i < numItems) {
+                let texture = this._allItems[i].app.create_icon_texture(subSize);
+                bin = new St.Bin({ child: texture });
+            } else {
+                bin = new St.Bin({ width: subSize, height: subSize });
+            }
+            layout.attach(bin, rtl ? (i + 1) % 2 : i % 2, Math.floor(i / 2), 1, 1);
+        }
+
+        return icon;
     },
 
     _onPan: function(action) {
