19928111ceee1fa9561084907521e0546fafea30
Joaquim Rocha <jrocha@endlessm.com>
2017-06-27 20:40:46 +0200

    appDisplay: Show the last page when adding a new folder
    Ideally the grid should know in which page the change took place and
    show it automatically which would avoid us having to navigate there
    directly. But until we have such a mechanism in place we need to
    show the last page before adding a new folder, otherwise the user may
    be in a different page and won't notice it.
    
    https://phabricator.endlessm.com/T17847

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -547,6 +547,11 @@ var AllView = new Lang.Class({
 
         this._addedFolderId = null;
         IconGridLayout.layout.connect('folder-added', (iconGridLayout, id) => {
+            // Go to last page; ideally the grid should know in
+            // which page the change took place and show it automatically
+            // which would avoid us having to navigate there directly
+            this.goToPage(this._grid.nPages() - 1);
+
             // Save the folder ID so we know which one was added
             // and set it to edit mode
             this._addedFolderId = id;
