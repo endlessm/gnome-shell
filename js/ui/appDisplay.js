7650dce1b4aa6827a292aad6e01ecc092a52b6a8
Mario Sanchez Prada <mario@endlessm.com>
2017-06-30 01:23:40 +0000

    dnd: Include the label when dragging icons, and keep it centered
    Currently, we are creating the drag actor using the same method that
    creates the icon for an app/folder, but that does not include the
    label. This patch fixes that by using BaseIcon directly and passing
    the relevant parameters to know how to create the icon and the label.
    
    https://phabricator.endlessm.com/T17661
    https://phabricator.endlessm.com/T18035

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1549,8 +1549,13 @@ class ViewIcon extends GObject.Object {
     }
 
     getDragActor() {
-        // Each subclass creates the actor returned here in different ways
-        throw new Error('Not implemented');
+        let iconParams = { createIcon: this._createIcon.bind(this),
+                           showLabel: (this.icon.label != null),
+                           setSizeManually: false };
+
+        let icon = new IconGrid.BaseIcon(this.getName(), iconParams);
+        icon.add_style_class_name('dnd');
+        return icon;
     }
 
     // Returns the original actor that should align with the actor
@@ -2138,10 +2143,6 @@ var AppIcon = GObject.registerClass({
 
     canDragOver(dest) {
         return true;
-    }
-
-    getDragActor() {
-        return this.app.create_icon_texture(this._iconSize);
     }
 
     shouldShowTooltip() {
