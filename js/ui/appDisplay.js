784cbc9cdadbb69d4c49c785d6fb028227c51e69
Mario Sanchez Prada <mario@endlessm.com>
2017-06-30 01:23:59 +0000

    dnd: Implement DnD integration for the App Center icon
    This patch adds the necessary code and resources to implement DnD
    operations over the App Center icon in the desktop grid, enabling
    the user to remove icons from the desktop and to undo the operation.
    
    Note that removing icons from inside folders is not supported yet,
    that will be added when implementing DnD support for folders.
    
    https://phabricator.endlessm.com/T17661

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -96,6 +96,8 @@ const EOS_ACTIVE_GRID_TRANSITION = 'easeInQuad';
 const EOS_INACTIVE_GRID_SATURATION = 1;
 const EOS_ACTIVE_GRID_SATURATION = 0;
 
+const EOS_DRAG_OVER_FOLDER_OPACITY = 128;
+
 const EOS_REPLACED_BY_KEY = 'X-Endless-Replaced-By';
 
 function _getCategories(info) {
@@ -678,7 +680,7 @@ const AllView = new Lang.Class({
             return;
         }
 
-        this._appCenterIcon = new AppCenterIcon();
+        this._appCenterIcon = new AppCenterIcon(this);
         this.addItem(this._appCenterIcon);
     },
 
@@ -902,6 +904,8 @@ const AllView = new Lang.Class({
         this._resetDragViewState();
 
         source.handleViewDragBegin();
+        if (this._appCenterIcon && (source.canDragOver(this._appCenterIcon)))
+            this._appCenterIcon.handleViewDragBegin();
     },
 
     _clearDragState: function(source) {
@@ -922,6 +926,8 @@ const AllView = new Lang.Class({
         this._resetDragViewState();
 
         source.handleViewDragEnd();
+        if (this._appCenterIcon && (source.canDragOver(this._appCenterIcon)))
+            this._appCenterIcon.handleViewDragEnd();
     },
 
     _onDragBegin: function(overview, source) {
@@ -959,7 +965,7 @@ const AllView = new Lang.Class({
 
         let draggingWithinFolder =
             this._currentPopup && (this._dragView == this._dragIcon.parentView);
-        let canDropPastEnd = draggingWithinFolder;
+        let canDropPastEnd = draggingWithinFolder || !this._appCenterIcon;
 
         // Ask grid can we drop here
         let [idx, cursorLocation] = this._dragView.canDropAt(dragEvent.x,
@@ -991,7 +997,7 @@ const AllView = new Lang.Class({
             if (hoverResult == DND.DragMotionResult.MOVE_DROP) {
                 // If we are hovering over a drop target, set its hover state
                 this._setDragHoverState(true);
-                dragEvent.dragActor.opacity = DRAG_OVER_FOLDER_OPACITY;
+                dragEvent.dragActor.opacity = EOS_DRAG_OVER_FOLDER_OPACITY;
             }
 
             return hoverResult;
@@ -1596,6 +1602,9 @@ const ViewIcon = new Lang.Class({
         this.showMenu = params.showMenu;
         this.parentView = params.parentView;
 
+        this.canDrop = false;
+        this.blockHandler = false;
+
         // Might be changed once the createIcon() method is called.
         this._iconSize = IconGrid.ICON_SIZE;
         this._iconState = ViewIconState.NORMAL;
@@ -1660,6 +1669,15 @@ const ViewIcon = new Lang.Class({
         return this._createIconFunc(this._iconSize);
     },
 
+    remove: function() {
+        this.blockHandler = true;
+        IconGridLayout.layout.removeIcon(this.getId(), true);
+        this.blockHandler = false;
+
+        this.handleViewDragEnd();
+        this.actor.hide();
+    },
+
     replaceText: function(newText) {
         if (!this.icon.label)
             return;
@@ -1690,8 +1708,10 @@ const ViewIcon = new Lang.Class({
     },
 
     handleViewDragEnd: function() {
-        this.iconState = ViewIconState.NORMAL;
-        this.resetDnDPlaceholder();
+        if (!this.blockHandler) {
+            this.iconState = ViewIconState.NORMAL;
+            this.resetDnDPlaceholder();
+        }
     },
 
     prepareForDrag: function() {
@@ -1704,6 +1724,10 @@ const ViewIcon = new Lang.Class({
 
     canDragOver: function(dest) {
         return false;
+    },
+
+    handleIconDrop: function(source) {
+        throw new Error('Not implemented');
     },
 
     getDragActor: function() {
@@ -2535,19 +2559,53 @@ const AppIconMenu = new Lang.Class({
 });
 Signals.addSignalMethods(AppIconMenu.prototype);
 
+const AppCenterIconState = {
+    EMPTY_TRASH: ViewIconState.NUM_STATES,
+    FULL_TRASH: ViewIconState.NUM_STATES + 1
+};
+
 const AppCenterIcon = new Lang.Class({
     Name: 'AppCenterIcon',
     Extends: AppIcon,
 
-    _init : function() {
+    _init : function(parentView) {
         let viewIconParams = { isDraggable: false,
-                               showMenu: false };
-        let iconParams = { editable: false };
+                               showMenu: false,
+                               parentView: parentView };
+
+        let iconParams = { createIcon: Lang.bind(this, this._createIcon) };
 
         let appSys = Shell.AppSystem.get_default();
         let app = appSys.lookup_app(EOS_APP_CENTER_ID);
 
         this.parent(app, viewIconParams, iconParams);
+
+        this.canDrop = true;
+    },
+
+    _setStyleClass: function(state) {
+        if (state == AppCenterIconState.EMPTY_TRASH) {
+            this.actor.remove_style_class_name('trash-icon-full');
+            this.actor.add_style_class_name('trash-icon-empty');
+        } else if (state == AppCenterIconState.FULL_TRASH) {
+            this.actor.remove_style_class_name('trash-icon-empty');
+            this.actor.add_style_class_name('trash-icon-full');
+        } else {
+            this.actor.remove_style_class_name('trash-icon-empty');
+            this.actor.remove_style_class_name('trash-icon-full');
+        }
+    },
+
+    _createIcon: function(iconSize) {
+        // Set the icon image as a background via CSS,
+        // and return an empty icon to satisfy the caller
+        this._setStyleClass(this.iconState);
+
+        if (this.iconState != ViewIconState.NORMAL)
+            return new St.Icon({ icon_size: iconSize });
+
+        // In normal state we chain up to the parent to get the default icon.
+        return this.parent(iconSize);
     },
 
     getId: function() {
@@ -2557,4 +2615,21 @@ const AppCenterIcon = new Lang.Class({
     getName: function() {
         return _("More Apps");
     },
+
+    handleViewDragBegin: function() {
+        this.iconState = AppCenterIconState.EMPTY_TRASH;
+        this.replaceText(_("Delete"));
+    },
+
+    setDragHoverState: function(state) {
+        let appCenterIconState = state ?
+            AppCenterIconState.FULL_TRASH : AppCenterIconState.EMPTY_TRASH;
+        this.iconState = appCenterIconState;
+    },
+
+    handleIconDrop: function(source) {
+        source.remove();
+        this.handleViewDragEnd();
+        return true;
+    }
 });
