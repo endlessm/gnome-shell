2bb3d92d2cf30ac9b60946df73254363bb73ebe5
Mario Sanchez Prada <mario@endlessm.com>
2017-06-30 01:23:59 +0000

    dnd: Implement DnD integration for the App Center icon
    This patch adds the necessary code and resources to implement DnD
    operations over the App Center icon in the desktop grid, enabling
    the user to remove icons from the desktop and to undo the operation.
    
    Note that removing icons from inside folders is not supported yet,
    that will be added when implementing DnD support for folders.
    
    https://phabricator.endlessm.com/T17661

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -538,7 +538,7 @@ var AllView = class AllView extends BaseAppView {
             return;
         }
 
-        this._appCenterIcon = new AppCenterIcon();
+        this._appCenterIcon = new AppCenterIcon(this);
         this.addItem(this._appCenterIcon);
     }
 
@@ -757,6 +757,8 @@ var AllView = class AllView extends BaseAppView {
         this._resetDragViewState();
 
         source.handleViewDragBegin();
+        if (this._appCenterIcon && (source.canDragOver(this._appCenterIcon)))
+            this._appCenterIcon.handleViewDragBegin();
     }
 
     _clearDragState(source) {
@@ -777,6 +779,8 @@ var AllView = class AllView extends BaseAppView {
         this._resetDragViewState();
 
         source.handleViewDragEnd();
+        if (this._appCenterIcon && (source.canDragOver(this._appCenterIcon)))
+            this._appCenterIcon.handleViewDragEnd();
     }
 
     _onDragBegin(overview, source) {
@@ -810,7 +814,7 @@ var AllView = class AllView extends BaseAppView {
 
         let draggingWithinFolder =
             this._currentPopup && (this._dragView == this._dragIcon.parentView);
-        let canDropPastEnd = draggingWithinFolder;
+        let canDropPastEnd = draggingWithinFolder || !this._appCenterIcon;
 
         // Ask grid can we drop here
         let [idx, cursorLocation] = this._dragView.canDropAt(dragEvent.x,
@@ -1467,6 +1471,9 @@ class ViewIcon extends GObject.Object {
         this.showMenu = params.showMenu;
         this.parentView = params.parentView;
 
+        this.canDrop = false;
+        this.blockHandler = false;
+
         // Might be changed once the createIcon() method is called.
         this._iconSize = IconGrid.ICON_SIZE;
         this._iconState = ViewIconState.NORMAL;
@@ -1539,6 +1546,15 @@ class ViewIcon extends GObject.Object {
         return this._createIconFunc(this._iconSize);
     }
 
+    remove() {
+        this.blockHandler = true;
+        IconGridLayout.layout.removeIcon(this.getId(), true);
+        this.blockHandler = false;
+
+        this.handleViewDragEnd();
+        this.actor.hide();
+    }
+
     replaceText(newText) {
         if (!this.icon.label)
             return;
@@ -1569,8 +1585,10 @@ class ViewIcon extends GObject.Object {
     }
 
     handleViewDragEnd() {
-        this.iconState = ViewIconState.NORMAL;
-        this.resetDnDPlaceholder();
+        if (!this.blockHandler) {
+            this.iconState = ViewIconState.NORMAL;
+            this.resetDnDPlaceholder();
+        }
     }
 
     prepareForDrag() {
@@ -1583,6 +1601,10 @@ class ViewIcon extends GObject.Object {
 
     canDragOver(dest) {
         return false;
+    }
+
+    handleIconDrop(source) {
+        throw new Error('Not implemented');
     }
 
     getDragActor() {
@@ -1669,7 +1691,7 @@ var FolderIcon = GObject.registerClass({
     }
 
     getAppIds() {
-        return this.view.getAllItems().map(function(item) {
+        return this.view.getAllItems().map(item => {
             return item.id;
         });
     }
@@ -2365,17 +2387,49 @@ var SystemActionIcon = class SystemActionIcon extends Search.GridSearchResult {
     }
 };
 
+const AppCenterIconState = {
+    EMPTY_TRASH: ViewIconState.NUM_STATES,
+    FULL_TRASH: ViewIconState.NUM_STATES + 1
+};
+
 var AppCenterIcon = GObject.registerClass(
 class AppCenterIcon extends AppIcon {
-    _init() {
+    _init(parentView) {
         let viewIconParams = { isDraggable: false,
-                               showMenu: false };
-        let iconParams = { editable: false };
+                               showMenu: false,
+                               parentView: parentView };
+
+        let iconParams = { createIcon: this._createIcon.bind(this) };
 
         let appSys = Shell.AppSystem.get_default();
         let app = appSys.lookup_app(EOS_APP_CENTER_ID);
 
         super._init(app, viewIconParams, iconParams);
+        this.canDrop = true;
+    }
+    _setStyleClass(state) {
+        if (state == AppCenterIconState.EMPTY_TRASH) {
+            this.actor.remove_style_class_name('trash-icon-full');
+            this.actor.add_style_class_name('trash-icon-empty');
+        } else if (state == AppCenterIconState.FULL_TRASH) {
+            this.actor.remove_style_class_name('trash-icon-empty');
+            this.actor.add_style_class_name('trash-icon-full');
+        } else {
+            this.actor.remove_style_class_name('trash-icon-empty');
+            this.actor.remove_style_class_name('trash-icon-full');
+        }
+    }
+
+    _createIcon(iconSize) {
+        // Set the icon image as a background via CSS,
+        // and return an empty icon to satisfy the caller
+        this._setStyleClass(this.iconState);
+
+        if (this.iconState != ViewIconState.NORMAL)
+            return new St.Icon({ icon_size: iconSize });
+
+        // In normal state we chain up to the parent to get the default icon.
+        return super._createIcon	(iconSize);
     }
 
     getId() {
@@ -2384,5 +2438,22 @@ class AppCenterIcon extends AppIcon {
 
     getName() {
         return this.app.get_generic_name();
+    }
+
+    handleViewDragBegin() {
+        this.iconState = AppCenterIconState.EMPTY_TRASH;
+        this.replaceText(_("Delete"));
+    }
+
+    setDragHoverState(state) {
+        let appCenterIconState = state ?
+            AppCenterIconState.FULL_TRASH : AppCenterIconState.EMPTY_TRASH;
+        this.iconState = appCenterIconState;
+    }
+
+    handleIconDrop(source) {
+        source.remove();
+        this.handleViewDragEnd();
+        return true;
     }
 });
