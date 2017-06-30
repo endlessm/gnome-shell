555a144461c2aa93040b92387e1d0e3b66825fa3
Mario Sanchez Prada <mario@endlessm.com>
2017-06-30 23:42:32 +0100

    overview: Don't hide the overview on startup when re-laying it out
    In a similar fashion to what we did in EOS 3.1, split the relayout()
    method in two, so that we can force a relayout on initialization of
    the overview without forcing a call to hide(), which we don't want
    to do since our shell starts right in overview mode.
    
    https://phabricator.endlessm.com/T17789

--- js/ui/overview.js
+++ js/ui/overview.js
@@ -259,7 +259,7 @@ var Overview = new Lang.Class({
         this.viewSelector.connect('page-changed', this._onPageChanged.bind(this));
         Main.layoutManager.connect('startup-prepared', this._onStartupPrepared.bind(this));
         Main.layoutManager.connect('monitors-changed', Lang.bind(this, this._relayout));
-        this._relayout();
+        this._relayoutNoHide();
     },
 
     addSearchProvider: function(provider) {
@@ -400,6 +400,10 @@ var Overview = new Lang.Class({
         // when it is next shown.
         this.hide();
 
+        this._relayoutNoHide();
+    },
+
+    _relayoutNoHide: function () {
         if (!Main.layoutManager.primaryMonitor)
             return;
 
