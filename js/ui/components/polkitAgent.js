e065d9f640250a004d7623d15e1c41adc12874e2
Joaquim Rocha <jrocha@endlessm.com>
2017-07-19 14:08:20 +0200

    polkitAgent: Do not set initial focus in the password entry
    The password entry can be hidden if the user has no password set, but
    setting the initial focus to it will result in the entry responding to
    the "activate" and signals.
    Besides, with the logic that the dialog follows, setting the initial
    focus in the entry is redundant, because it the focus is also set in
    the entry upon showing it.
    
    https://phabricator.endlessm.com/T17269

--- js/ui/components/polkitAgent.js
+++ js/ui/components/polkitAgent.js
@@ -118,7 +118,6 @@ var AuthenticationDialog = new Lang.Class({
 
         this._passwordBox.add(this._workSpinner.actor);
 
-        this.setInitialKeyFocus(this._passwordEntry);
         this._passwordBox.hide();
 
         this._errorMessageLabel = new St.Label({ style_class: 'prompt-dialog-error-label' });
