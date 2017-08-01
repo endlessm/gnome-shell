4e9ec03719c68c54ee13a186a19b6b73266a8420
Mario Sanchez Prada <mario@endlessm.com>
2017-08-01 15:45:22 +0100

    Revert "appDisplay: Temporarily disabling DnD of applications inside a folder"
    Finally, this enables DnD inside folders now that everything else is in place.
    
    This reverts commit edbd2e8bc1ed1d07969d0e2b2ef460f4648b927c.
    
    https://phabricator.endlessm.com/T18012

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1626,12 +1626,6 @@ const FolderView = new Lang.Class({
 
     getViewId: function() {
         return this._folderIcon.getId();
-    },
-
-    canDropAt: function(x, y, canDropPastEnd) {
-        // FIXME: Temporarily disable dragging elements around inside a
-        // folder, as it's causing the GrabHelper to crash for some reason.
-        return [-1, IconGrid.CursorLocation.DEFAULT];
     }
 });
 
