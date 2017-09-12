863b3639ffca46f87bb386a1cd97a7a8b6de8c48
Mario Sanchez Prada <mario@endlessm.com>
2017-09-12 15:55:03 +0100

    overview: Show dialog when toggling to showing windows with no apps running
    When there are no applications opened and the user presses the super key,
    clicks on the Endless button or uses the hot corner, we show a dialog with
    a message informing about that.
    
    https://phabricator.endlessm.com/T17227

--- js/ui/overview.js
+++ js/ui/overview.js
@@ -5,6 +5,7 @@ const GLib = imports.gi.GLib;
 const Gtk = imports.gi.Gtk;
 const Meta = imports.gi.Meta;
 const Mainloop = imports.mainloop;
+const Pango = imports.gi.Pango;
 const Signals = imports.signals;
 const Lang = imports.lang;
 const St = imports.gi.St;
@@ -17,6 +18,7 @@ const LayoutManager = imports.ui.layout;
 const Lightbox = imports.ui.lightbox;
 const Main = imports.ui.main;
 const MessageTray = imports.ui.messageTray;
+const ModalDialog = imports.ui.modalDialog;
 const Monitor = imports.ui.monitor;
 const OverviewControls = imports.ui.overviewControls;
 const Panel = imports.ui.panel;
@@ -36,6 +38,8 @@ var SHADE_ANIMATION_TIME = .20;
 var DND_WINDOW_SWITCH_TIMEOUT = 750;
 
 var OVERVIEW_ACTIVATION_TIMEOUT = 0.5;
+
+var NO_WINDOWS_OPEN_DIALOG_TIMEOUT = 2000; // ms
 
 var ShellInfo = new Lang.Class({
     Name: 'ShellInfo',
@@ -106,6 +110,55 @@ var ShellInfo = new Lang.Class({
     }
 });
 
+var NoWindowsDialog = new Lang.Class({
+    Name: 'NoWindowsDialog',
+    Extends: ModalDialog.ModalDialog,
+
+    _init: function() {
+        this.parent({ styleClass: 'prompt-dialog',
+                      shellReactive: true,
+                      destroyOnClose: false });
+
+        this._timeoutId = 0;
+
+        let descriptionLabel = new St.Label({ style_class: 'prompt-dialog-headline headline',
+                                              text: _('No apps are open') });
+        descriptionLabel.clutter_text.line_wrap = true;
+        descriptionLabel.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
+
+        this.contentLayout.add(descriptionLabel,
+                               { x_fill: false,
+                                 y_fill: false,
+                                 x_align: St.Align.MIDDLE,
+                                 y_align: St.Align.MIDDLE });
+
+        this._group.connect('key-press-event', (event) => {
+            this.close(global.get_current_time());
+            return Clutter.EVENT_PROPAGATE;
+        });
+    },
+
+    show: function() {
+        if (this._timeoutId != 0)
+            Mainloop.source_remove(this._timeoutId);
+
+        this._timeoutId =
+            Mainloop.timeout_add(NO_WINDOWS_OPEN_DIALOG_TIMEOUT, () => {
+                this.hide();
+                return GLib.SOURCE_REMOVE;
+            });
+        this.open(global.get_current_time());
+    },
+
+    hide: function() {
+        if (this._timeoutId != 0) {
+            Mainloop.source_remove(this._timeoutId);
+            this._timeoutId = 0;
+        }
+        this.close(global.get_current_time());
+    },
+});
+
 var Overview = new Lang.Class({
     Name: 'Overview',
 
@@ -115,6 +168,7 @@ var Overview = new Lang.Class({
 
         Main.sessionMode.connect('updated', Lang.bind(this, this._sessionUpdated));
         this._sessionUpdated();
+        this._noWindowsDialog = new NoWindowsDialog();
     },
 
     _createOverview: function() {
@@ -520,6 +574,11 @@ var Overview = new Lang.Class({
             return;
         }
 
+        if (!Main.workspaceMonitor.hasActiveWindows) {
+            this._noWindowsDialog.show();
+            return;
+        }
+
         if (!Main.workspaceMonitor.hasVisibleWindows) {
             // There are active windows but all of them are hidden, so activate
             // the most recently used one before hiding the overview.
@@ -537,8 +596,17 @@ var Overview = new Lang.Class({
         if (this.isDummy)
             return;
 
-        if (!this.visible ||
-            this.viewSelector.getActivePage() !== ViewSelector.ViewPage.WINDOWS) {
+        if (!this.visible) {
+            this.showWindows();
+            return;
+        }
+
+        if (!Main.workspaceMonitor.hasActiveWindows) {
+            this._noWindowsDialog.show();
+            return;
+        }
+
+        if (this.viewSelector.getActivePage() !== ViewSelector.ViewPage.WINDOWS) {
             this.showWindows();
             return;
         }
@@ -691,6 +759,9 @@ var Overview = new Lang.Class({
         }
 
         this._shown = false;
+
+        // Hide the 'No windows dialog' in case it is open
+        this._noWindowsDialog.hide();
 
         this._animateNotVisible();
         this._syncGrab();
