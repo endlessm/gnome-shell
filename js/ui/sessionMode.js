5b2336d1512ba9197bb33233b62d1c5aea938e0f
Mario Sanchez Prada <mario@endlessm.com>
2017-09-14 15:26:44 +0100

    endlessButton: Add the Endless button to the bottom panel
    It acts as the Super key, toggling between the apps view
    and the last opened window if there is at least one running
    application, otherwise it shows a "No apps are open" message.
    
    https://phabricator.endlessm.com/T17227
    https://phabricator.endlessm.com/T17435
    https://phabricator.endlessm.com/T17506
    https://phabricator.endlessm.com/T17520
    https://phabricator.endlessm.com/T18327
    https://phabricator.endlessm.com/T18565
    https://phabricator.endlessm.com/T20325

--- js/ui/sessionMode.js
+++ js/ui/sessionMode.js
@@ -97,7 +97,7 @@ const _modes = {
                      'keyring', 'autorunManager', 'automountManager'],
 
         panel: {
-            left: ['appIcons'],
+            left: ['endlessButton', 'appIcons'],
             center: [],
             right: ['a11y', 'keyboard', 'aggregateMenu', 'dateMenu']
         }
