b527e4dfb8c17c339379608b872136f8b4ae1c82
Mario Sanchez Prada <mario@endlessm.com>
2017-09-20 13:47:55 +0100

    appDisplay: Don't animate icons one by one when opening a folder
    That animation doesn't feel right when opening a folder with many
    apps inside, specially on slow computers where we skip many frames.
    
    https://phabricator.endlessm.com/T18330

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1514,11 +1514,6 @@ const FolderView = new Lang.Class({
         Util.ensureActorVisibleInScrollView(this.actor, actor);
     },
 
-    // Overriden from BaseAppView
-    animate: function(animationDirection) {
-        this._grid.animatePulse(animationDirection);
-    },
-
     createFolderIcon: function(size) {
         let layout = new Clutter.GridLayout();
         let icon = new St.Widget({ layout_manager: layout,
