endlessm/gnome-shell

commit 9d638286fa9489ab1a13db3bce45f411a50d64fb

appDisplay: Only defer redisplay of the icon grid on startup
In Endless, we have the apps grid (or a clone of it) showing up
all the time, so we can't defer updating it until the grid's actor
is mapped because that would create an artificial delay between an
icon gets added to the grid and the moment is made visible if the
"real" grid (not the clone) is not visible at that moment.

We still need to defer the call to redisplay() when starting up,
though, so that we don't draw anything until the grid is mapped.

https://phabricator.endlessm.com/T19596

js/ui/appDisplay.js
@@ -587,14 +587,14 @@ const AllView = new Lang.Class({
         this._redisplayWorkId = Main.initializeDeferredWork(this.actor, Lang.bind(this, this._redisplay));
 
         Shell.AppSystem.get_default().connect('installed-changed', Lang.bind(this, function() {
-            Main.queueDeferredWork(this._redisplayWorkId);
+            this._redisplay();
         }));
 
         IconGridLayout.layout.connect('changed', Lang.bind(this, function() {
-            Main.queueDeferredWork(this._redisplayWorkId);
+            this._redisplay();
         }));
         global.settings.connect('changed::' + EOS_ENABLE_APP_CENTER_KEY, Lang.bind(this, function() {
-            Main.queueDeferredWork(this._redisplayWorkId);
+            this._redisplay();
         }));
 
         this._addedFolderId = null;

commit 47e8bf3016b484974511e93c6ba2f1f521ad915f

folderView: Queue icon update
This approach avoids a broken grab (exactly what was caused
with 9d638286fa), and is consistent with what AllView does.

This is, of course, hiding a symptom of a deeper problem,
but that's the farthest we can go for now. Ideally, this
commit will be dropped with the next rebase.

https://phabricator.endlessm.com/T20087

js/ui/appDisplay.js
@@ -1772,9 +1772,10 @@ const FolderView = new Lang.Class({
             if (!this.iconsNeedRedraw())
                 return;
 
-            this._redisplay();
-
-            this._folderIcon.icon.reloadIcon();
+            if (!this._recreateIconWorkId)
+                this._recreateIconWorkId = Main.initializeDeferredWork(this._folderIcon.actor, Lang.bind(this, this._redisplay));
+            else
+                Main.queueDeferredWork(this._recreateIconWorkId);
         }));
     },
 
@@ -1782,6 +1783,7 @@ const FolderView = new Lang.Class({
         this.removeAll();
         this.addIcons();
         this.updateNoAppsLabelVisibility();
+        this._folderIcon.icon.reloadIcon();
     },
 
     _createItemIcon: function(item) {