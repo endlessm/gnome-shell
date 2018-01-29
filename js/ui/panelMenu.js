30ead4e1277529d106d4c0dd76296fd5d59cf4e9
Mario Sanchez Prada <mario@endlessm.com>
2018-01-29 18:35:55 +0000

    bottomPanel: Reorganize elements and style in the bottom panel
      - Removed Activities and AppMenu buttoms from the panel.
      - Moved the clock button to the right of the panel and adjust style.
      - Make sure that the clock button is only shown in the 'user' session mode.
      - Adapt style of boxpointers and popup menus.
      - Adapt style of the system indicators area.
      - Change transparency of the bottom panel to be 90% opaque (upstream is 35%).
    
    https://phabricator.endlessm.com/T17880
    https://phabricator.endlessm.com/T20327

--- js/ui/panelMenu.js
+++ js/ui/panelMenu.js
@@ -137,6 +137,13 @@ var Button = new Lang.Class({
              event.type() == Clutter.EventType.BUTTON_PRESS))
             this.menu.toggle();
 
+        if ((event.get_state() & Clutter.ModifierType.BUTTON1_MASK) ||
+            (event.get_state() & Clutter.ModifierType.BUTTON2_MASK) ||
+            (event.get_state() & Clutter.ModifierType.BUTTON3_MASK))
+            this.actor.add_style_pseudo_class('clicked');
+        else
+            this.actor.remove_style_pseudo_class('clicked');
+
         return Clutter.EVENT_PROPAGATE;
     },
 
