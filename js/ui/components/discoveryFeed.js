d31951fe4a39dee1131a009dff6c8264076d857f
Mario Sanchez Prada <mario@endlessm.com>
2018-01-31 15:50:44 +0000

    discoveryFeed: Add Discovery Feed component
    Also, add integration with the side component.
    
    https://phabricator.endlessm.com/T18601

---
+++ js/ui/components/discoveryFeed.js
@@ -0,0 +1,47 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+
+const { GObject } = imports.gi;
+
+const Main = imports.ui.main;
+const SideComponent = imports.ui.sideComponent;
+
+const DISCOVERY_FEED_NAME = 'com.endlessm.DiscoveryFeed';
+const DISCOVERY_FEED_PATH = '/com/endlessm/DiscoveryFeed';
+
+const DiscoveryFeedIface = '<node> \
+<interface name="' + DISCOVERY_FEED_NAME + '"> \
+<method name="show"> \
+  <arg type="u" direction="in" name="timestamp"/> \
+</method> \
+<method name="hide"> \
+  <arg type="u" direction="in" name="timestamp"/> \
+</method> \
+<property name="Visible" type="b" access="read"/> \
+</interface> \
+</node>';
+
+var DiscoveryFeed = GObject.registerClass(
+class DiscoveryFeed extends SideComponent.SideComponent {
+    _init() {
+        super._init(DiscoveryFeedIface, DISCOVERY_FEED_NAME, DISCOVERY_FEED_PATH);
+    }
+
+    enable() {
+        super.enable();
+        Main.discoveryFeed = this;
+    }
+
+    disable() {
+        super.disable();
+        Main.discoveryFeed = null;
+    }
+
+    callShow(timestamp) {
+        this.proxy.showRemote(timestamp);
+    }
+
+    callHide(timestamp) {
+        this.proxy.hideRemote(timestamp);
+    }
+});
+var Component = DiscoveryFeed;
