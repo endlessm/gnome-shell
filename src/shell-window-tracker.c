3a4df343c1d1a575b935a8fb04354a10a73256d2
Mario Sanchez Prada <mario@endlessm.com>
2018-02-07 15:42:53 +0000

    coding: Add CodeView assets and logic

--- src/shell-window-tracker.c
+++ src/shell-window-tracker.c
@@ -32,6 +32,8 @@
 #define SIDE_COMPONENT_ROLE "eos-side-component"
 #define SPEEDWAGON_ROLE "eos-speedwagon"
 
+#define BUILDER_WINDOW "org.gnome.Builder"
+
 /**
  * SECTION:shell-window-tracker
  * @short_description: Associate windows with applications
@@ -50,6 +52,8 @@ struct _ShellWindowTracker
 
   /* <MetaWindow * window, ShellApp *app> */
   GHashTable *window_to_app;
+
+  MetaWindow *coding_app;
 };
 
 G_DEFINE_TYPE (ShellWindowTracker, shell_window_tracker, G_TYPE_OBJECT);
@@ -380,6 +384,21 @@ get_app_for_window (ShellWindowTracker    *tracker,
   /* Side components don't have an associated app */
   if (g_strcmp0 (meta_window_get_role (window), SIDE_COMPONENT_ROLE) == 0)
     return NULL;
+
+  /* We do want to associate the GNOME Builder window with
+   * an open app of a coding session */
+  if (g_strcmp0 (meta_window_get_flatpak_id (window), BUILDER_WINDOW) == 0 &&
+      tracker->coding_app)
+    {
+      result = g_hash_table_lookup (tracker->window_to_app, tracker->coding_app);
+      shell_window_tracker_untrack_coding_app_window (tracker);
+
+      if (result != NULL)
+        {
+          g_object_ref (result);
+          return result;
+        }
+    }
 
   transient_for = meta_window_get_transient_for (window);
   if (transient_for != NULL)
@@ -852,6 +871,34 @@ gboolean
 shell_window_tracker_is_speedwagon_window (MetaWindow *window)
 {
   return g_strcmp0 (meta_window_get_role (window), SPEEDWAGON_ROLE) == 0;
+}
+
+/**
+ * shell_window_tracker_track_coding_app_window:
+ * @tracker: An app monitor instance
+ * @app_window: A #MetaWindow
+ *
+ * Track a coding app to pair with an associated GNOME Builder window.
+ *
+ */
+void
+shell_window_tracker_track_coding_app_window (ShellWindowTracker *tracker,
+                                              MetaWindow *app_window)
+{
+  tracker->coding_app = app_window;
+}
+
+/**
+ * shell_window_tracker_untrack_coding_app_window:
+ * @tracker: An app monitor instance
+ *
+ * Untrack the coding app.
+ *
+ */
+void
+shell_window_tracker_untrack_coding_app_window (ShellWindowTracker *tracker)
+{
+  tracker->coding_app = NULL;
 }
 
 /* sn_startup_sequence_ref returns void, so make a
