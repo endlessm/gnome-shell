cb19ae6bf9118e3e6be828da121d1c1c9a9cf424
Mario Sanchez Prada <mario@endlessm.com>
2018-02-14 20:51:10 +0000

    EditableLabel: Allow editing desktop icon names
    Previously, Endless OS shell allowed users to rename
    the desktop icons and folders. With the GNOME Shell
    rebase, however, that feature was lost and we cannot
    rename them anymore.
    
    This commit brings back the code to edit desktop folders
    only, since we're no longer interested in allowing users
    to rename applications, as that presented several issues.
    
    https://phabricator.endlessm.com/T17660
    https://phabricator.endlessm.com/T17847
    https://phabricator.endlessm.com/T19575

--- js/ui/iconGrid.js
+++ js/ui/iconGrid.js
@@ -12,6 +12,8 @@ const Params = imports.misc.params;
 const Tweener = imports.ui.tweener;
 const Main = imports.ui.main;
 
+const EditableLabel = imports.ui.editableLabel;
+
 var ICON_SIZE = 64;
 var MIN_ICON_SIZE = 16;
 
@@ -41,6 +43,7 @@ var BaseIcon = new Lang.Class({
         params = Params.parse(params, { createIcon: null,
                                         createExtraIcons: null,
                                         setSizeManually: false,
+                                        editable: false,
                                         showLabel: true });
 
         let styleClass = 'overview-icon';
@@ -88,7 +91,12 @@ var BaseIcon = new Lang.Class({
         this._layeredIcon.add_actor(shadow);
 
         if (params.showLabel) {
-            this.label = new St.Label({ text: label });
+            if (params.editable)
+                this.label = new EditableLabel.EditableLabel({ text: label,
+                                                               style_class: 'overview-icon-label' });
+            else
+                this.label = new St.Label({ text: label,
+                                            style_class: 'overview-icon-label' });
             box.add_actor(this.label);
         } else {
             this.label = null;
