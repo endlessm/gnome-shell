40bcb2b34b57106adda53ba93db6f8d93a82ec05
Mario Sanchez Prada <mario@endlessm.com>
2018-02-14 11:24:46 +0000

    taskbar: Add the application icons taskbar and related style changes
    Also, this commit also includes support for HiDPI resolutions.
    
    https://phabricator.endlessm.com/T17433
    https://phabricator.endlessm.com/T17482
    https://phabricator.endlessm.com/T18350
    https://phabricator.endlessm.com/T18851
    
     * 13-03-2019: squashed with beb2ef63f
        appIconBar: use the right method to specify packing properties
    
        ClutterContainer.add() is patched in js/ui/environment.js to allow passing
        child properties. We use ClutterActor.add_child() though, which doesn't
        allow that, and causes warnings on the terminal when that code is
        reached.
    
        https://phabricator.endlessm.com/T21741

--- js/ui/sessionMode.js
+++ js/ui/sessionMode.js
@@ -99,7 +99,7 @@ const _modes = {
                      'keyring', 'autorunManager', 'automountManager'],
 
         panel: {
-            left: [],
+            left: ['appIcons'],
             center: [],
             right: ['a11y', 'keyboard', 'aggregateMenu', 'dateMenu']
         }
