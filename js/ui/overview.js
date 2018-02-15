127c338b6def960ebde9f0308b200002ee9f0d55
Mario Sanchez Prada <mario@endlessm.com>
2018-02-15 15:32:59 +0000

    Remove the dash, we are no longer using it on EOS
    Do not ever instance the dash or any of its related classes, and
    adapt code and callers accordingly where needed. Specifically, this
    required to remove any occurrendce of 'showAppsButton', which was used
    from many different places, and won't longer be available now that we
    are not using the dash, where it used to live.
    
    Also, note that I actually removed some code to prevent _redisplay()
    from being executed, since otherwise we could hit an error when trying
    to process the icons, that won't necessarily exist.
    
    https://phabricator.endlessm.com/T18032

--- js/ui/overview.js
+++ js/ui/overview.js
@@ -235,7 +235,6 @@ var Overview = new Lang.Class({
 
         // Create controls
         this._controls = new OverviewControls.ControlsManager(this._searchEntry);
-        this._dash = this._controls.dash;
         this.viewSelector = this._controls.viewSelector;
 
         // Add our same-line elements after the search entry
@@ -248,13 +247,6 @@ var Overview = new Lang.Class({
                                         opacity: 0 });
 
         this._overview.add_actor(this._panelGhost);
-
-        // TODO - recalculate everything when desktop size changes
-        this.dashIconSize = this._dash.iconSize;
-        this._dash.connect('icon-size-changed',
-                           Lang.bind(this, function() {
-                               this.dashIconSize = this._dash.iconSize;
-                           }));
 
         this.viewSelector.connect('page-changed', this._onPageChanged.bind(this));
         Main.layoutManager.connect('startup-prepared', this._onStartupPrepared.bind(this));
@@ -767,10 +759,6 @@ var Overview = new Lang.Class({
 
     getActivePage: function() {
         return this.viewSelector.getActivePage();
-    },
-
-    getShowAppsButton: function() {
-        return this._dash.showAppsButton;
     }
 });
 Signals.addSignalMethods(Overview.prototype);
