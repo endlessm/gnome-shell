cc5e46056fb7bdf556aa75c903188321d5de3ae8
Mario Sanchez Prada <mario@endlessm.com>
2018-03-22 18:56:37 +0000

    payg: Make sure to initialize the users list after startup has completed
    This is not necessary upstream because the dialog has always going to be
    created before the startup process has completed, but in PAYG machines
    this is no longer true since the PAYG dialog will show up before than
    the login screen if the machine is locked.
    
    But in our case it's necessary to do an extra check and initialize the
    list of users explicitly if the startup process has already finished,
    otherwise an empty login screen will be shown after unlocking the machine.
    
    https://phabricator.endlessm.com/T21609

--- js/gdm/loginDialog.js
+++ js/gdm/loginDialog.js
@@ -527,6 +527,10 @@ var LoginDialog = new Lang.Class({
         // focus later
         this._startupCompleteId = Main.layoutManager.connect('startup-complete',
                                                              Lang.bind(this, this._updateDisableUserList));
+
+        // With PAYG machines this class can be created AFTER the startup process.
+        if (!Main.layoutManager.startingUp)
+            this._updateDisableUserList();
     },
 
     _getBannerAllocation: function (dialogBox) {
