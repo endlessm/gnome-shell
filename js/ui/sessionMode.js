f4cd7971c8334c5191c946f0275024fc35bd60f5
Mario Sanchez Prada <mario@endlessm.com>
2018-03-26 14:09:05 +0100

    payg: Integrate the PAYG unlock screen with the login screen
    We need to create a new session mode for this instead of simply
    reusing the other mode for PAYG since we will be in 'greeter mode'
    there, and make sure to push such mode from the right place.
    
    https://phabricator.endlessm.com/T21609

--- js/ui/sessionMode.js
+++ js/ui/sessionMode.js
@@ -55,6 +55,11 @@ const _modes = {
         panelStyle: 'login-screen'
     },
 
+    'gdm-unlock-dialog-payg': {
+        parentMode: 'gdm',
+        unlockDialog: imports.ui.paygUnlockDialog.PaygUnlockDialog,
+    },
+
     'lock-screen': {
         isLocked: true,
         isGreeter: undefined,
