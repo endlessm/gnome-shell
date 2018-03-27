fe61c34b2ee31edfc0e8af981d16ade342ea2423
Mario Sanchez Prada <mario@endlessm.com>
2018-03-27 17:24:21 +0100

    payg: Wait for the PaygManager before showing the screenshield's dialog
    The login screen showed on boot is actually managed by ScreenShield,
    which shows it if needed as LoginDialog is set as the unlockDialog for
    the 'gdm' session mode. This is a problem with PAYG since we now need
    to wait for the PaygManager to be fully initialized before deciding
    whether to push an extra session mode on top (to show the PAYG unlock
    dialog if needed), so we need to make sure we do that.
    
    https://phabricator.endlessm.com/T21608

--- js/ui/main.js
+++ js/ui/main.js
@@ -241,7 +241,17 @@ function _initializeUI() {
 
     if (sessionMode.isGreeter && screenShield) {
         layoutManager.connect('startup-prepared', function() {
-            screenShield.showDialog();
+            // We can't show the login dialog (which is managed by the
+            // screenshield) until the PaygManager is initializd, since
+            // we need to check whether the machine is PAYG-locked first.
+            if (paygManager.initialized) {
+                screenShield.showDialog();
+            } else {
+                let paygManagerId = paygManager.connect('initialized', () => {
+                    screenShield.showDialog();
+                    paygManager.disconnect(paygManagerId);
+                });
+            }
         });
     }
 
