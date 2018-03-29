0ca7d3a8c46e13b5b858a4a648b03404f3b0ffc2
Mario Sanchez Prada <mario@endlessm.com>
2018-03-29 12:16:44 +0100

    payg: Show an explicit error message when timing out
    The 'Unknown error' message would cover this, but I think it's
    interesting in this case to show something else for debugging
    purposes, since an user pointing to us that a time out happened
    will immediately direct us into a D-Bus communication issue.
    
    https://phabricator.endlessm.com/T21806

--- js/ui/paygUnlockDialog.js
+++ js/ui/paygUnlockDialog.js
@@ -420,6 +420,8 @@ class PaygUnlockDialog extends GObject.Object {
             this._setErrorMessage(_("Invalid code"));
         } else if (error.matches(PaygManager.PaygErrorDomain, PaygManager.PaygError.CODE_ALREADY_USED)) {
             this._setErrorMessage(_("Code already used"));
+        } else if (error.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.TIMED_OUT)) {
+            this._setErrorMessage(_("Time exceeded while verifying the code"));
         } else {
             // We don't consider any other error here (and we don't consider DISABLED explicitly,
             // since that should not happen), but still we need to show something to the user.
