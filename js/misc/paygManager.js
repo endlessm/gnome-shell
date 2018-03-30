340b21482cd0cbc5e85f80fb96ef0155b056afb5
Mario Sanchez Prada <mario@endlessm.com>
2018-03-30 10:17:31 +0100

    payg: Support the new property com.endlessm.Payg1.RateLimitEndTime
    That will allow the shell to show some extra information to the user,
    suggesting how much time it will be needed to wait.
    
    https://phabricator.endlessm.com/T21808

--- js/misc/paygManager.js
+++ js/misc/paygManager.js
@@ -38,6 +38,7 @@ const EOS_PAYG_IFACE = '<node> \
 <signal name="Expired" /> \
 <property name="ExpiryTime" type="t" access="read"/> \
 <property name="Enabled" type="b" access="read"/> \
+<property name="RateLimitEndTime" type="t" access="read"/> \
 </interface> \
 </node>';
 
@@ -74,6 +75,7 @@ var PaygManager = GObject.registerClass({
 
         this._enabled = false;
         this._expiryTime = 0;
+        this._rateLimitEndTime = 0;
 
         this._codeExpiredId = 0;
         this._propertiesChangedId = 0;
@@ -105,6 +107,7 @@ var PaygManager = GObject.registerClass({
             // selecting the session mode to construct the right unlock dialog.
             this._enabled = this._proxy.Enabled;
             this._expiryTime = this._proxy.ExpiryTime;
+            this._rateLimitEndTime = this._proxy.RateLimitEndTime;
 
             this._propertiesChangedId = this._proxy.connect('g-properties-changed', this._onPropertiesChanged.bind(this));
             this._codeExpiredId = this._proxy.connectSignal('Expired', this._onCodeExpired.bind(this));
@@ -121,6 +124,9 @@ var PaygManager = GObject.registerClass({
 
         if (propsDict.hasOwnProperty('ExpiryTime'))
             this._setExpiryTime(this._proxy.ExpiryTime);
+
+        if (propsDict.hasOwnProperty('RateLimitEndTime'))
+            this._setRateLimitEndTime(this._proxy.RateLimitEndTime);
     }
 
     _setEnabled(value) {
@@ -137,6 +143,14 @@ var PaygManager = GObject.registerClass({
 
         this._expiryTime = value;
         this.emit('expiry-time-changed', this._expiryTime);
+    }
+
+    _setRateLimitEndTime(value) {
+        if (this._rateLimitEndTime === value)
+            return;
+
+        this._rateLimitEndTime = value;
+        this.emit('rate-limit-end-time-changed', this._rateLimitEndTime);
     }
 
     _onCodeExpired(proxy) {
@@ -165,6 +179,10 @@ var PaygManager = GObject.registerClass({
     get expiryTime() {
         return this._expiryTime;
 
+    }
+
+    get rateLimitEndTime() {
+        return this._rateLimitEndTime;
     }
 
     get isLocked() {
