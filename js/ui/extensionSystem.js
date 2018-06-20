9b1d089f333d69717f0b2656a327daf605386dfd
Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>
2018-06-20 19:22:06 +0200

    Don't allow endless mode extension to update
    Ensure that no update is proposed or loaded if sideloaded (always
    prefer system version) on the ubuntu session.
    
    We want to ensure that the default code running is going through
    our QA and security team process than being loaded from a 3rd
    party website.
    
    Also, that will enable us to upload newer versions on GNOME
    extension website while still letting older Endless release versions
    running expected extension version.
    
    Origin: ubuntu
    Modified by: Endless
    
    Forwarded: https://bugzilla.gnome.org/show_bug.cgi?id=789852

--- js/ui/extensionSystem.js
+++ js/ui/extensionSystem.js
@@ -5,6 +5,7 @@ const { GLib, Gio, GObject, Shell, St } = imports.gi;
 const ByteArray = imports.byteArray;
 const Signals = imports.signals;
 
+const Desktop = imports.misc.desktop;
 const ExtensionDownloader = imports.ui.extensionDownloader;
 const ExtensionUtils = imports.misc.extensionUtils;
 const FileUtils = imports.misc.fileUtils;
@@ -399,6 +400,10 @@ var ExtensionManager = class {
         this.loadExtension(newExtension);
     }
 
+    isModeExtension(uuid) {
+        return this._getModeExtensions().indexOf(uuid) !== -1;
+    }
+
     _callExtensionInit(uuid) {
         if (!Main.sessionMode.allowExtensions)
             return false;
@@ -579,6 +584,10 @@ var ExtensionManager = class {
             let type = dir.has_prefix(perUserDir)
                 ? ExtensionType.PER_USER
                 : ExtensionType.SYSTEM;
+            if (Desktop.is('endless') && this.isModeExtension(uuid) && type === ExtensionType.PER_USER) {
+                log(`Found user extension ${uuid}, but not loading from ${dir.get_path()} directory as part of session mode.`);
+                return;
+            }
             try {
                 extension = this.createExtensionObject(uuid, dir, type);
             } catch (e) {
