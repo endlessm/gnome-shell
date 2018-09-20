730800ea33dbae53f8d381071557c82328126e1f
Will Thompson <wjt@endlessm.com>
2018-09-20 15:02:09 +0100

    shell-app-system: refactor duplicated X-Endless-Alias code
    We add apps to the alias_to_id map both when scanning all installed apps
    in scan_alias_to_id(), and when looking up an app in
    shell_app_system_lookup_app() if it is not already in the id_to_app map.
    
    At first glance it may look like I have removed the check in
    shell_app_system_lookup_app() which prevents overwriting an existing
    alias. In fact the check is wrong: it looks for the bare app ID
    ("com.example.Foo"), but adds the desktop file name
    ("com.example.Foo.desktop").
    
    https://phabricator.endlessm.com/T22596

--- src/shell-app-system.c
+++ src/shell-app-system.c
@@ -119,6 +119,21 @@ static void shell_app_system_class_init(ShellAppSystemClass *klass)
                   SHELL_TYPE_APP);
 }
 
+static void
+add_aliases (ShellAppSystem  *self,
+             GDesktopAppInfo *info)
+{
+  ShellAppSystemPrivate *priv = self->priv;
+  const char *id = g_app_info_get_id (G_APP_INFO (info));
+  const char *alias;
+
+  alias = g_desktop_app_info_get_string (info, X_ENDLESS_ALIAS_KEY);
+  if (alias != NULL)
+    {
+      char *desktop_alias = g_strconcat (alias, ".desktop", NULL);
+      g_hash_table_insert (priv->alias_to_id, desktop_alias, g_strdup (id));
+    }
+}
 
 static void
 scan_alias_to_id (ShellAppSystem *self)
@@ -130,19 +145,7 @@ scan_alias_to_id (ShellAppSystem *self)
 
   apps = g_app_info_get_all ();
   for (l = apps; l != NULL; l = l->next)
-    {
-      GAppInfo *info = l->data;
-      const char *alias, *id;
-      g_autofree char *desktop_alias = NULL;
-
-      id = g_app_info_get_id (info);
-      alias = g_desktop_app_info_get_string (G_DESKTOP_APP_INFO (info), X_ENDLESS_ALIAS_KEY);
-      if (alias == NULL)
-        continue;
-
-      desktop_alias = g_strconcat (alias, ".desktop", NULL);
-      g_hash_table_insert (priv->alias_to_id, g_strdup (desktop_alias), g_strdup (id));
-    }
+    add_aliases (self, G_DESKTOP_APP_INFO (l->data));
 
   g_list_free_full (apps, g_object_unref);
 }
@@ -422,7 +425,7 @@ shell_app_system_lookup_app (ShellAppSystem   *self,
 {
   ShellAppSystemPrivate *priv = self->priv;
   ShellApp *app;
-  GDesktopAppInfo *info;
+  g_autoptr(GDesktopAppInfo) info = NULL;
   g_autofree char *alias = NULL;
 
   app = g_hash_table_lookup (priv->id_to_app, id);
@@ -435,14 +438,6 @@ shell_app_system_lookup_app (ShellAppSystem   *self,
 
   app = _shell_app_new (info);
   g_hash_table_insert (priv->id_to_app, (char *) shell_app_get_id (app), app);
-  g_object_unref (info);
-
-  alias = g_desktop_app_info_get_string (info, X_ENDLESS_ALIAS_KEY);
-  if (alias != NULL && g_hash_table_lookup (priv->alias_to_id, alias) == NULL)
-    {
-      g_autofree char *desktop_alias = g_strconcat (alias, ".desktop", NULL);
-      g_hash_table_insert (priv->alias_to_id, g_strdup (desktop_alias), g_strdup (id));
-    }
 
   return app;
 }
