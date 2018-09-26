fb375c214c75e4ac4e6aad4f850e41c729d45c33
Cosimo Cecchi <cosimo@endlessm.com>
2018-09-26 10:38:42 -0700

    Revert "coding: Filter out certain coding apps if we're not enabled"
    This reverts commit 3356bd5ba410dec430006b3080e7a89ae5326c7b.

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1577,11 +1577,6 @@ var AppSearchProvider = new Lang.Class({
         let groups = Shell.AppSystem.search(query);
         let usage = Shell.AppUsage.get_default();
         let results = [];
-        let codingEnabled = global.settings.get_boolean('enable-coding-game');
-        let codingApps = [
-            'com.endlessm.Coding.Chatbox.desktop',
-            'eos-shell-extension-prefs.desktop'
-        ];
         let replacementMap = {};
 
         groups.forEach(function(group) {
@@ -1592,10 +1587,6 @@ var AppSearchProvider = new Lang.Class({
 
                 // exclude links that are not part of the desktop grid
                 if (!(app && app.should_show() && !(isLink && !isOnDesktop)))
-                    return false;
-
-                // exclude coding related apps if coding game is not enabled
-                if (!codingEnabled && codingApps.indexOf(appID) > -1)
                     return false;
 
                 if (app && app.should_show()) {
