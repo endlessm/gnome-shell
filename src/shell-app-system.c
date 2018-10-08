98b50e98e3bbec597829425176dc156554caa7ea
Will Thompson <wjt@endlessm.com>
2018-10-08 13:31:24 +0100

    shell-app-system: don't update aliases from lookup_app
    At startup, and when GAppInfoMonitor::changed is emitted, the lookup
    table of aliases is cleared and rebuilt from scratch, looking at all
    desktop files returned by g_app_info_get_all(). This scans the same set
    of directories that g_desktop_app_info_new(id) will search. It follows
    that if we reach this point of shell_app_system_lookup_app(), where the
    app was not in the id_to_app cache (it's lazily initialized) but was
    found by g_desktop_app_info_new(), then the aliases from the app info
    have already been scanned and added to priv->alias_to_id. (It's also a
    bit of a conceptually funny one: if something in the shell already knows
    the app's new ID, almost by definion we don't care about its aliases.)
    
    Unfortunately, this call to add_aliases() was not harmless.  When an
    alias is found in priv->alias_to_id, shell_app_system_lookup_alias()
    calls shell_app_system_lookup_app() with that id (a value in
    priv->alias_to_id). Then shell_app_system_lookup_app() calls
    add_aliases() with that id.  For each alias in the desktop file,
    add_aliases() calls:
    
      g_hash_table_insert (priv->alias_to_id,
                           alias,
                           g_strdup (id));
    
    When the key already exists in the table, g_hash_table_insert() frees
    the old value and replaces it with the new one (g_strdup (id)). Since
    the old value was id itself, after inserting one alias, id is now a
    pointer to freed memory. Any desktop file with >1 alias (of which we
    have many) would cause the shell to call g_strdup() on a dangling
    pointer.
    
    https://phabricator.endlessm.com/T23973
    (cherry picked from commit aaa4acae1b09379d8a479da9f028b9f6b6c2d988)
    Signed-off-by: Robert McQueen <rob@endlessm.com>

--- src/shell-app-system.c
+++ src/shell-app-system.c
@@ -430,7 +430,6 @@ shell_app_system_lookup_app (ShellAppSystem   *self,
 
   app = _shell_app_new (info);
   g_hash_table_insert (priv->id_to_app, (char *) shell_app_get_id (app), app);
-  add_aliases (self, id, info);
 
   return app;
 }
