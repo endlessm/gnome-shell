99da8a2e1d76c37baf6d60b71bf164967a0cd6d2
Cosimo Cecchi <cosimo@endlessm.com>
2018-10-10 17:28:43 -0700

    notificationDaemon: separate out GtkNotification creation
    This way, source subclasses can easily use a notification subclass
    if different functionality is required.

--- js/ui/notificationDaemon.js
+++ js/ui/notificationDaemon.js
@@ -713,6 +713,10 @@ var GtkNotificationDaemonAppSource = new Lang.Class({
         return new FdoApplicationProxy(Gio.DBus.session, this._appId, this._objectPath, callback);
     },
 
+    _createNotification: function(params) {
+        return new GtkNotificationDaemonNotification(this, params);
+    },
+
     activateAction: function(actionId, target) {
         this._createApp(function (app, error) {
             if (error == null)
@@ -741,7 +745,7 @@ var GtkNotificationDaemonAppSource = new Lang.Class({
         if (this._notifications[notificationId])
             this._notifications[notificationId].destroy();
 
-        let notification = new GtkNotificationDaemonNotification(this, notificationParams);
+        let notification = this._createNotification(notificationParams);
         notification.connect('destroy', Lang.bind(this, function() {
             delete this._notifications[notificationId];
         }));
