fca8e23701bf4d5173c3cc691406b81bbff23eae
Joaquim Rocha <jrocha@endlessm.com>
2018-10-24 11:10:57 +0200

    shellDBus: Fix showing the app grid
    Calling the ShowApplications() method was displaying the app windows
    view instead of the app grid.
    
    https://phabricator.endlessm.com/T24218

--- js/ui/shellDBus.js
+++ js/ui/shellDBus.js
@@ -106,7 +106,7 @@ var GnomeShell = class {
     }
 
     ShowApplications() {
-        Main.overview.viewSelector.showApps();
+        Main.overview.showApps();
     }
 
     GrabAcceleratorAsync(params, invocation) {
