1bac0d50e2cf250b51ab45159c8338b55bcd7936
Joaquim Rocha <jrocha@endlessm.com>
2018-11-07 20:00:18 +0100

    clubhouse: Make the Quest View move left with the Clubhouse window
    From the new UX specs, the Quest View banner should move left along with
    the Clubhouse window when the latter is opened.
    
    These changes implement that by using a new helper class that makes it
    easy to track the Clubhouse's window and to get its position.
    This new class reuses some of the code from the ClubhouseButtonManager
    and will replace that code in a later commit.
    
    https://phabricator.endlessm.com/T24365

--- js/ui/components/clubhouse.js
+++ js/ui/components/clubhouse.js
@@ -55,6 +55,93 @@ const ClubhouseIface =
   </interface> \
 </node>';
 
+var getClubhouseWindowTracker = (function () {
+    let clubhouseWindowTracker;
+    return function () {
+        if (!clubhouseWindowTracker)
+            clubhouseWindowTracker = new ClubhouseWindowTracker();
+        return clubhouseWindowTracker;
+    };
+}());
+
+var ClubhouseWindowTracker = new Lang.Class({
+    Name: 'ClubhouseWindowTracker',
+    Extends: GObject.Object,
+
+    Signals: {
+        'window-changed': {},
+    },
+
+    _init: function() {
+        this.parent();
+
+        this._windowActor = null;
+
+        // If the Clubhouse is open and the Shell is restarted, then global.get_window_actors()
+        // will still not have any contents when components are initialized, and there's no "map"
+        // signal emitted from the Window Manager either. So instead of relying on the map signal
+        // (which would allow us to track windows individually instead of always having to check the
+        // current list of windows) we connect to the screen's "restacked" signal.
+        global.screen.connect('restacked', this._trackWindowActor.bind(this));
+
+        Main.overview.connect('showing', () => { this.emit('window-changed'); });
+        Main.overview.connect('hidden', () => { this.emit('window-changed'); });
+        Main.sessionMode.connect('updated', () => { this.emit('window-changed'); });
+    },
+
+    _getClubhouseActorFromWM: function() {
+        return global.get_window_actors().find((actor) => {
+            return (actor.meta_window.get_gtk_application_id() == CLUBHOUSE_ID);
+        });
+    },
+
+    _trackWindowActor: function() {
+        let actor = this._getClubhouseActorFromWM();
+        if (!actor)
+            return;
+
+        if (this._windowActor == actor)
+            return;
+
+        // Reset the current window actor
+        this._untrackWindowActor();
+
+        this._windowActor = actor;
+
+        this.emit('window-changed');
+
+        // Track Clubhouse's window actor to make the closeButton always show on top of
+        // the Clubhouse's window edge
+        this._notifyHandler = actor.connect('notify::x', () => {
+            this.emit('window-changed');
+        });
+
+        this._destroyHandler = actor.connect('destroy', () => {
+            this._untrackWindowActor();
+            this.emit('window-changed');
+        });
+    },
+
+    _untrackWindowActor: function() {
+        if (!this._windowActor)
+            return;
+
+        // Reset the current window actor
+        this._windowActor.disconnect(this._notifyHandler);
+        this._notifyHandler = 0;
+
+        this._windowActor.disconnect(this._destroyHandler);
+        this._destroyHandler = 0;
+
+        this._windowActor = null;
+    },
+
+    getWindowX: function() {
+        if (!Main.sessionMode.hasOverview || Main.overview.visible || this._windowActor == null)
+            return -1;
+        return this._windowActor.x;
+    },
+});
 
 var ClubhouseNotificationBanner = new Lang.Class({
     Name: 'ClubhouseNotificationBanner',
@@ -77,6 +164,13 @@ var ClubhouseNotificationBanner = new Lang.Class({
 
         // Always wrap the body's text
         this._expandedLabel.actor.add_style_class_name('clubhouse-quest-view-label');
+
+        this._clubhouseTrackerHandler =
+            getClubhouseWindowTracker().connect('window-changed', this.reposition.bind(this));
+
+        this.actor.connect('destroy', () => {
+            getClubhouseWindowTracker().disconnect(this._clubhouseTrackerHandler);
+        });
     },
 
     reposition: function() {
@@ -85,7 +179,15 @@ var ClubhouseNotificationBanner = new Lang.Class({
             return;
 
         let margin = 30;
-        this.actor.x = monitor.x + monitor.width - (this.actor.width + margin);
+
+        let clubhouseWindowX = getClubhouseWindowTracker().getWindowX();
+
+        if (clubhouseWindowX == -1)
+            this.actor.x = monitor.x + monitor.width;
+        else
+            this.actor.x = clubhouseWindowX;
+
+        this.actor.x -= this.actor.width + margin;
         this.actor.y = margin;
     },
 
