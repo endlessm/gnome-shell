713c49a50c8a53429d17436ee065b2ea909afac3
Fabian Orccon <cfoch.fabian@gmail.com>
2018-11-19 12:26:21 -0500

    codeView: Play sound when flip animation starts
    (cherry picked from commit 8546c3a825a726b72d427d223f5c41a9b5b7ed07)
    Signed-off-by: Robert McQueen <rob@endlessm.com>

--- js/ui/codeView.js
+++ js/ui/codeView.js
@@ -260,7 +260,6 @@ var WindowTrackingButton = new Lang.Class({
         this.parent(params);
 
         this.connect('notify::hover', this._onHoverChanged.bind(this));
-        this.connect('clicked', this._onClick.bind(this));
     },
 
     vfunc_allocate: function(box, flags) {
@@ -370,14 +369,6 @@ var WindowTrackingButton = new Lang.Class({
             }
         } else {
             this._stopHoverSound();
-        }
-    },
-
-    _onClick: function() {
-        if (!this.child.flipped) {
-            SoundServer.getDefault().play('shell/tracking-button/flip/click');
-        } else {
-            SoundServer.getDefault().play('shell/tracking-button/flip-inverse/click');
         }
     }
 });
@@ -1010,12 +1001,20 @@ var CodingSession = new Lang.Class({
         }
     },
 
+    _playAnimationSound: function(direction) {
+        if (direction == Gtk.DirectionType.LEFT)
+            SoundServer.getDefault().play('shell/tracking-button/flip/click');
+        else
+            SoundServer.getDefault().play('shell/tracking-button/flip-inverse/click');
+    },
+
     _completeAnimate: function(src, oldDst, newDst, direction, targetState) {
         this._animateToMidpoint(src,
                                 oldDst,
                                 newDst,
                                 direction);
         this._button.switchAnimation(direction, targetState);
+        this._playAnimationSound(direction);
     },
 
     _animateToMidpoint: function(src, oldDst, newDst, direction) {
