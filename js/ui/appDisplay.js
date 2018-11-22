a0061c461bb32ad6de5276fac18dfe2d4e72c8bf
Philip Withnall <withnall@endlessm.com>
2018-11-22 14:38:52 +0000

    js: Delay instantiation of IconGridLayout until it’s needed
    Previously, an IconGridLayout instance was created as soon as
    iconGridLayout.js was imported, as a constructor was called outside of a
    top-level function. This caused it to be created even when gnome-shell
    was running as Debian-gdm and showing only the login screen.
    
    Add a getDefault() singleton constructor, which allows construction of
    the IconGridLayout instance to when it’s actually needed.
    
    As it happens, the gnome-shell D-Bus interface (shellDBus.js) needs it,
    and is instantiated when the shell is run as Debian-gdm. That makes this
    patch have no functional changes — but we can at least now work on
    eliminating the D-Bus/IconGridLayout dependency from the greeter without
    other parts of the shell pulling it in.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://phabricator.endlessm.com/T24016

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -156,6 +156,8 @@ var BaseAppView = new Lang.Class({
                                                 padWithSpacing: true });
         params = Params.parse(params, { usePagination: false });
 
+        this._iconGridLayout = IconGridLayout.getDefault();
+
         if(params.usePagination)
             this._grid = new IconGrid.PaginatedIconGrid(gridParams);
         else
@@ -176,7 +178,7 @@ var BaseAppView = new Lang.Class({
     },
 
     _createItemForId: function(itemId) {
-        if (IconGridLayout.layout.iconIsFolder(itemId))
+        if (this._iconGridLayout.iconIsFolder(itemId))
             return Shell.DesktopDirInfo.new(itemId);
 
         return Shell.AppSystem.get_default().lookup_app(itemId);
@@ -188,13 +190,13 @@ var BaseAppView = new Lang.Class({
 
     getLayoutIds: function() {
         let viewId = this.getViewId();
-        return IconGridLayout.layout.getIcons(viewId).slice();
+        return this._iconGridLayout.getIcons(viewId).slice();
     },
 
     _trimInvisible: function(ids) {
         let appSystem = Shell.AppSystem.get_default();
         return ids.filter((id) => {
-            return IconGridLayout.layout.iconIsFolder(id) || appSystem.lookup_app(id) || (id == EOS_APP_CENTER_ID);
+            return this._iconGridLayout.iconIsFolder(id) || appSystem.lookup_app(id) || (id == EOS_APP_CENTER_ID);
         });
     },
 
@@ -738,7 +740,7 @@ var AllView = new Lang.Class({
         Shell.AppSystem.get_default().connect('installed-changed', Lang.bind(this, function() {
             Main.queueDeferredWork(this._redisplayWorkId);
         }));
-        IconGridLayout.layout.connect('changed', () => {
+        this._iconGridLayout.connect('changed', () => {
             Main.queueDeferredWork(this._redisplayWorkId);
         });
         global.settings.connect('changed::' + EOS_ENABLE_APP_CENTER_KEY, () => {
@@ -746,7 +748,7 @@ var AllView = new Lang.Class({
         });
 
         this._addedFolderId = null;
-        IconGridLayout.layout.connect('folder-added', (iconGridLayout, id) => {
+        this._iconGridLayout.connect('folder-added', (iconGridLayout, id) => {
             // Go to last page; ideally the grid should know in
             // which page the change took place and show it automatically
             // which would avoid us having to navigate there directly
@@ -822,7 +824,7 @@ var AllView = new Lang.Class({
 
         let itemId = item.get_id();
 
-        if (!IconGridLayout.layout.iconIsFolder(itemId)) {
+        if (!this._iconGridLayout.iconIsFolder(itemId)) {
             return new AppIcon(item,
                                { isDraggable: true,
                                  parentView: this },
@@ -1307,7 +1309,7 @@ var AllView = new Lang.Class({
             this._currentPopup.popdown();
         }
 
-        IconGridLayout.layout.repositionIcon(source.getId(), insertId, folderId);
+        this._iconGridLayout.repositionIcon(source.getId(), insertId, folderId);
         return true;
     },
 
@@ -1537,6 +1539,7 @@ var AppSearchProvider = new Lang.Class({
         this.isRemoteProvider = false;
         this.canLaunchSearch = false;
 
+        this._iconGridLayout = IconGridLayout.getDefault();
         this._systemActions = new SystemActions.getDefault();
     },
 
@@ -1583,7 +1586,7 @@ var AppSearchProvider = new Lang.Class({
             group = group.filter((appID) => {
                 let app = Gio.DesktopAppInfo.new(appID);
                 let isLink = appID.startsWith(EOS_LINK_PREFIX);
-                let isOnDesktop = IconGridLayout.layout.hasIcon(appID);
+                let isOnDesktop = this._iconGridLayout.hasIcon(appID);
 
                 // exclude links that are not part of the desktop grid
                 if (!(app && app.should_show() && !(isLink && !isOnDesktop)))
@@ -1607,9 +1610,9 @@ var AppSearchProvider = new Lang.Class({
         results = results.concat(this._systemActions.getMatchingActions(terms));
 
         // resort to keep results on the desktop grid before the others
-        results = results.sort(function(a, b) {
-            let hasA = a === EOS_APP_CENTER_ID || IconGridLayout.layout.hasIcon(a);
-            let hasB = b === EOS_APP_CENTER_ID || IconGridLayout.layout.hasIcon(b);
+        results = results.sort((a, b) => {
+            let hasA = a === EOS_APP_CENTER_ID || this._iconGridLayout.hasIcon(a);
+            let hasB = b === EOS_APP_CENTER_ID || this._iconGridLayout.hasIcon(b);
 
             return hasB - hasA;
         });
@@ -1689,14 +1692,14 @@ var FolderView = new Lang.Class({
         });
 
         this._redisplayFolderWorkId = Main.initializeDeferredWork(this._folderIcon.actor, () => { this._redisplay(); });
-        let layoutChangedId = IconGridLayout.layout.connect('changed', () => {
+        let layoutChangedId = this._iconGridLayout.connect('changed', () => {
             // AllView only checks for the toplevel icons, not those inside folders.
             Main.queueDeferredWork(this._redisplayFolderWorkId);
         });
 
         this._folderIcon.actor.connect('destroy', () => {
             // The deferred work will not be valid after the folderIcon is destroyed.
-            IconGridLayout.layout.disconnect(layoutChangedId);
+            this._iconGridLayout.disconnect(layoutChangedId);
         });
 
         // Don't call _redisplay() here, since that will call reloadIcon() which, besides
@@ -1909,6 +1912,8 @@ var ViewIcon = new Lang.Class({
         this.blockHandler = false;
 
         this._scaleInId = 0;
+
+        this._iconGridLayout = IconGridLayout.getDefault();
 
         // Might be changed once the createIcon() method is called.
         this._iconSize = IconGrid.ICON_SIZE;
@@ -2148,7 +2153,7 @@ var ViewIcon = new Lang.Class({
 
     remove: function() {
         this.blockHandler = true;
-        IconGridLayout.layout.removeIcon(this.getId(), true);
+        this._iconGridLayout.removeIcon(this.getId(), true);
         this.blockHandler = false;
 
         this.handleViewDragEnd();
@@ -2424,7 +2429,7 @@ var FolderIcon = new Lang.Class({
 
     handleIconDrop: function(source) {
         // Move the source icon into this folder
-        IconGridLayout.layout.appendIcon(source.getId(), this.getId());
+        this._iconGridLayout.appendIcon(source.getId(), this.getId());
         return true;
     },
 
