601d04a94f3b6073d5488b9e48c005bd6949bfb9
Philip Withnall <withnall@endlessm.com>
2018-11-22 15:03:41 +0000

    js: Add support for parental controls filtering to the desktop
    Filter the apps shown on the desktop and in search results according to
    whether they are blacklisted by the user’s parental controls.
    
    This will not dynamically update the filter during the user’s session,
    as we assume that cannot happen — see parentalControlsManager.js for the
    rationale.
    
    Note that since the desktop layout is serialised to a GSetting, if an
    app is present on the desktop, is then filtered (due to the user’s
    parental controls being changed, for example), and then the user updates
    their desktop in some other way, the app will need to be manually
    re-added to the desktop if it’s ever removed from the parental controls
    blacklist.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://phabricator.endlessm.com/T24016

---
+++ js/misc/parentalControlsManager.js
@@ -0,0 +1,81 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+//
+// Copyright (C) 2018 Endless Mobile, Inc.
+//
+// This is a GNOME Shell component to wrap the interactions over
+// D-Bus with the eos-parental-controls library.
+//
+// Licensed under the GNU General Public License Version 2
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+const { EosParentalControls, Gio, GLib, GObject, Shell } = imports.gi;
+
+const Gettext = imports.gettext;
+const Signals = imports.signals;
+
+let _singleton = null;
+
+function getDefault() {
+    if (_singleton == null)
+        _singleton = new ParentalControlsManager();
+
+    return _singleton;
+}
+
+// A manager class which provides cached access to the constructing user’s
+// parental controls settings. There are currently no notifications of changes
+// to those settings, as it’s assumed that only an administrator can change a
+// user’s parental controls, the administrator themselves doesn’t have any
+// parental controls, and two users cannot be logged in at the same time (we
+// don’t allow fast user switching).
+var ParentalControlsManager = GObject.registerClass(
+class ParentalControlsManager extends GObject.Object {
+    _init() {
+        super._init();
+
+        log('Getting parental controls for user ' + Shell.util_get_uid ());
+
+        try {
+            this._appFilter = EosParentalControls.get_app_filter(null,
+                                                                 Shell.util_get_uid (),
+                                                                 false, null);
+        } catch (e) {
+            logError(e, 'Failed to get parental controls settings');
+        }
+    }
+
+    // Calculate whether the given app (a Gio.DesktopAppInfo) should be shown
+    // on the desktop, in search results, etc. The app should be shown if:
+    //  - The .desktop file doesn’t say it should be hidden.
+    //  - The executable from the .desktop file’s Exec line isn’t blacklisted in
+    //    the user’s parental controls.
+    //  - None of the flatpak app IDs from the X-Flatpak and the
+    //    X-Flatpak-RenamedFrom lines are blacklisted in the user’s parental
+    //    controls.
+    shouldShowApp(appInfo) {
+        // Quick decision?
+        if (!appInfo.should_show())
+            return false;
+
+        // Have we finished initialising yet?
+        if (!this._appFilter) {
+            log('Warning: Hiding app because parental controls not yet initialised: ' + appInfo.get_id());
+            return false;
+        }
+
+        return this._appFilter.is_appinfo_allowed(appInfo);
+    }
+});
