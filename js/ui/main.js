40b2150d929a86b3ad213891d6d941adbe092c0c
Philip Withnall <withnall@endlessm.com>
2018-11-22 15:03:41 +0000

    js: Add support for parental controls filtering to the desktop
    Filter the apps shown on the desktop and in search results according to
    whether they are blacklisted by the user’s parental controls.
    
    This will not dynamically update the filter during the user’s session,
    as we assume that cannot happen — see parentalControlsManager.js for the
    rationale.
    
    Note that since the desktop layout is serialised to a GSetting, if an
    app is present on the desktop, is then filtered (due to the user’s
    parental controls being changed, for example), and then the user updates
    their desktop in some other way, the app will need to be manually
    re-added to the desktop if it’s ever removed from the parental controls
    blacklist.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://phabricator.endlessm.com/T24016

--- js/ui/main.js
+++ js/ui/main.js
@@ -22,6 +22,7 @@ const Overview = imports.ui.overview;
 const PadOsd = imports.ui.padOsd;
 const Panel = imports.ui.panel;
 const Params = imports.misc.params;
+const ParentalControlsManager = imports.misc.parentalControlsManager;
 const RunDialog = imports.ui.runDialog;
 const Layout = imports.ui.layout;
 const LoginManager = imports.misc.loginManager;
@@ -128,6 +129,9 @@ function start() {
 
     sessionMode = new SessionMode.SessionMode();
     sessionMode.connect('updated', _sessionUpdated);
+
+    // Initialize ParentalControlsManager before the UI
+    ParentalControlsManager.getDefault();
 
     St.Settings.get().connect('notify::gtk-theme', _loadDefaultStylesheet);
     _initializeUI();
