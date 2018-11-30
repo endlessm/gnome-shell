32c1c6c19b8c352429265b238e83be25d615158b
Fabian Orccon <cfoch.fabian@gmail.com>
2018-11-30 18:36:57 -0500

    clubhouse: Hook up sounds for entry button

---
+++ js/ui/soundable.js
@@ -0,0 +1,84 @@
+const Lang = imports.lang;
+const St = imports.gi.St;
+const GObject = imports.gi.GObject;
+const SoundServer = imports.misc.soundServer;
+
+
+var Button = new Lang.Class({
+    Name: 'SoundableButton',
+    Extends: St.Button,
+    Properties: {
+        'click-sound-event-id': GObject.ParamSpec.string(
+            'click-sound-event-id',
+            '',
+            '',
+            GObject.ParamFlags.READWRITE,
+            ''
+        ),
+        'hover-sound-event-id': GObject.ParamSpec.string(
+            'hover-sound-event-id',
+            '',
+            '',
+            GObject.ParamFlags.READWRITE,
+            ''
+        ),
+        'stop-hover-sound-on-click': GObject.ParamSpec.boolean(
+            'stop-hover-sound-on-click',
+            '',
+            '',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT,
+            false
+        )
+    },
+
+    _init: function(params) {
+        this._hoverSoundItem = null;
+
+        this.parent(params);
+
+        this.connect('clicked', this._onClicked.bind(this));
+        this.connect('notify::hover', this._onHoverChanged.bind(this));
+    },
+
+    set hover_sound_event_id(value) {
+        if (this._hoverSoundItem &&
+            this._hoverSoundItem.name === value)
+            return;
+
+        this._stopHoverSound();
+        this._hoverSoundItem = null;
+
+        if (value)
+            this._hoverSoundItem = new SoundServer.SoundItem(value);
+    },
+
+    get hover_sound_event_id() {
+        if (this._hoverSoundItem)
+            return this._hoverSoundItem.name;
+        return null;
+    },
+
+    _onClicked: function() {
+        if (this.stop_hover_sound_on_click)
+            this._stopHoverSound();
+        if (this.click_sound_event_id)
+            SoundServer.getDefault().play(this.click_sound_event_id);
+    },
+
+    _onHoverChanged: function() {
+        if (this.hover)
+            this._startHoverSound();
+        else
+            this._stopHoverSound();
+    },
+
+    _startHoverSound() {
+        if (this._hoverSoundItem)
+            this._hoverSoundItem.play();
+    },
+
+    _stopHoverSound() {
+        if (this._hoverSoundItem)
+            this._hoverSoundItem.stop();
+    }
+});
