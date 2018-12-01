endlessm/gnome-shell

commit b31067f480e946d80f16c3f5536503f6d324c753

clubhouse: Don't stop the quest if the notification source is destroyed
The Clubhouse notification source is getting destroyed when it runs out
of notifications, and it was calling the action to stop the ongoing
quest in the Clubhouse. This was a problem when stopping a quest from
the Clubhouse and immediately start another, because the Clubhouse
itself stops the ongoing quest (and removes the notification) which
would end up destroying the notification source and thus getting the
"stop quest" action called again, while at this point the new quest may
be running already.
i.e. the new quest would be quickly stopped because of the "stop quest"
action called by the Clubhouse notification source destruction.

This patch stops this behavior by not calling the mentioned action when
the Clubhouse notification source is destroyed. This was unnecessary
in any case, because the notifications are the ones that should
eventually dismiss the quest, and not the notification source itself.

https://phabricator.endlessm.com/T24658

js/ui/components/clubhouse.js
@@ -800,7 +800,6 @@ var ClubhouseComponent = new Lang.Class({
         this._clubhouseSource = new ClubhouseNotificationSource(CLUBHOUSE_ID);
         this._clubhouseSource.connect('notify', Lang.bind(this, this._onNotify));
         this._clubhouseSource.connect('destroy', () => {
-            this._clubhouseSource.activateAction('stop-quest', null);
             this._clubhouseSource = null;
         })
 

commit d01cafb3db55e1e19d30c13c6f1698a78c7f8bd7

clubhouse: use source from notification to activate action
Instead of relying on this._clubhouseSource.

Both the clubhouse source and the clubhouse component connect to the
destroy signal of the notification; the clubhouse source callback will
destroy the source when there are no notifications, while the
clubhouse component will possibly activate the stop-quest action.

We set the source to NULL in the clubhouse component when it's
destroyed, but that happens before the other callback for the
stop-quest action to be activated is reached. Since the stop-quest
action is currently activated on the clubhouse source reference owned
by the clubhouse component, the result is that we never manage to
activate the action when the notification banner is destroyed.

This commit fixes the issue by using the source reference owned by the
notification in order to activate stop-quest, which will still be
alive during the callback.

This is a regression from commit
b31067f480e946d80f16c3f5536503f6d324c753.

https://phabricator.endlessm.com/T24698

js/ui/components/clubhouse.js
@@ -885,7 +885,7 @@ var ClubhouseComponent = new Lang.Class({
             notification.connect('destroy', (notification, reason) => {
                 if (reason != MessageTray.NotificationDestroyedReason.REPLACED &&
                     reason != MessageTray.NotificationDestroyedReason.SOURCE_CLOSED)
-                    this._dismissQuest();
+                    this._dismissQuest(notification.source);
 
                 this._clearQuestBanner();
             });
@@ -917,10 +917,10 @@ var ClubhouseComponent = new Lang.Class({
         this._syncVisibility();
     },
 
-    _dismissQuest: function() {
+    _dismissQuest: function(source) {
         // Stop the quest since the banner has been dismissed
-        if (this.proxy.g_name_owner && this._clubhouseSource)
-            this._clubhouseSource.activateAction('stop-quest', null);
+        if (this.proxy.g_name_owner)
+            source.activateAction('stop-quest', null);
 
         this._isRunningQuest = false;
     },