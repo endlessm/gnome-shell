a0bb81e4d7570704f4bc95dc35e1d2c0ce7bce3b
Manuel Quiñones <manuel.por.aca@gmail.com>
2018-12-03 17:12:30 -0300

    animation: Allow reusing frames in the format
    Animations can now define which frames to play by passing a
    framesInfo. This allows repeating frames, and define different delays
    for each frame in the list. This superceeds the setFrameTimeous
    method.
    
    https://phabricator.endlessm.com/T24468

--- js/ui/animation.js
+++ js/ui/animation.js
@@ -16,12 +16,12 @@ var Animation = new Lang.Class({
         this.actor = new St.Bin();
         this.actor.connect('destroy', Lang.bind(this, this._onDestroy));
         this._speed = speed;
-        this._frameTimeouts = [this._speed];
 
         this._isLoaded = false;
         this._isPlaying = false;
         this._timeoutId = 0;
-        this._frame = 0;
+        this._framesInfo = [];
+        this._frameIndex = 0;
 
         let scaleFactor = St.ThemeContext.get_for_stage(global.stage).scale_factor;
         this._animations = St.TextureCache.get_default().load_sliced_image (file, width, height, scaleFactor,
@@ -34,10 +34,10 @@ var Animation = new Lang.Class({
             // Set the frame to be the previous one, so when we update it
             // when play is called, it shows the current frame instead of
             // the next one.
-            if (this._frame == 0)
-                this._frame = this._animations.get_n_children() - 1;
+            if (this._frameIndex == 0)
+                this._frameIndex = this._framesInfo.length - 1;
             else
-                this._frame -= 1;
+                this._frameIndex -= 1;
 
             this._update();
         }
@@ -54,25 +54,37 @@ var Animation = new Lang.Class({
         this._isPlaying = false;
     },
 
+    _getCurrentFrame: function() {
+        return this._framesInfo[this._frameIndex];
+    },
+
+    _getCurrentFrameActor: function() {
+        let currentFrame = this._getCurrentFrame();
+        return this._animations.get_child_at_index(currentFrame.frameIndex);
+    },
+
+    _getCurrentDelay: function() {
+        let currentFrame = this._getCurrentFrame();
+        return currentFrame.frameDelay;
+    },
+
     _showFrame: function(frame) {
-        let oldFrameActor = this._animations.get_child_at_index(this._frame);
+        let oldFrameActor = this._getCurrentFrameActor();
         if (oldFrameActor)
             oldFrameActor.hide();
 
-        this._frame = (frame % this._animations.get_n_children());
+        this._frameIndex = (frame % this._framesInfo.length);
 
-        let newFrameActor = this._animations.get_child_at_index(this._frame);
+        let newFrameActor = this._getCurrentFrameActor();
         if (newFrameActor)
             newFrameActor.show();
     },
 
     _update: function() {
-        this._showFrame(this._frame + 1);
+        this._showFrame(this._frameIndex + 1);
 
         // Show the next frame after the timeout of the current one
-        let currentFrameTimeoutIndex = this._frame % this._frameTimeouts.length;
-        this._timeoutId = GLib.timeout_add(GLib.PRIORITY_LOW,
-                                           this._frameTimeouts[currentFrameTimeoutIndex],
+        this._timeoutId = GLib.timeout_add(GLib.PRIORITY_LOW, this._getCurrentDelay(),
                                            Lang.bind(this, this._update));
 
         GLib.Source.set_name_by_id(this._timeoutId, '[gnome-shell] this._update');
@@ -83,6 +95,13 @@ var Animation = new Lang.Class({
     _animationsLoaded: function() {
         this._isLoaded = this._animations.get_n_children() > 0;
 
+        if (this._isLoaded && this._framesInfo.length === 0) {
+            // If a custom sequence of frames wasn't provided,
+            // fallback to play the frames in sequence.
+            for (let i = 0; i < this._animations.get_n_children(); i++)
+                this._framesInfo.push({'frameIndex': i, 'frameDelay': this._speed});
+        }
+
         if (this._isPlaying)
             this.play();
     },
@@ -91,17 +110,14 @@ var Animation = new Lang.Class({
         this.stop();
     },
 
-    setFrameTimeouts: function(timeoutList) {
-        if (timeoutList.length == 0)
-            throw new Error('Cannot set an empty list as the frame-timeouts for animation!');
-
+    setFramesInfo: function(framesInfo) {
         let wasPlaying = this._isPlaying;
         this.stop();
 
-        this._frameTimeouts = timeoutList;
+        this._framesInfo = framesInfo;
 
-        // If the animation was playing, we continue to play it here (where it will
-        // use the new timeouts)
+        // If the animation was playing, we continue to play it here
+        // (where it will use the new frames)
         if (wasPlaying)
             this.play();
     }
