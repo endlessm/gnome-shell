354d84edf1a3d0c68a7e70c1304dafb4f7f590a9
Travis Reitter <travis.reitter@endlessm.com>
2018-12-03 16:43:53 -0800

    status/paygo: new applet for PayGo account details and actions
    This applet shows the time remaining for an active PayGo account and
    adds an obvious place to add relevant actions.
    
    The applet hides if this system is not set up for PayGo.
    
    https://phabricator.endlessm.com/T24125

--- js/ui/panel.js
+++ js/ui/panel.js
@@ -770,6 +770,11 @@ class AggregateMenu extends PanelMenu.Button {
         } else {
             this._bluetooth = null;
         }
+        if (Config.HAVE_PAYGO) {
+            this._paygo = new imports.ui.status.paygo.Indicator();
+        } else {
+            this._paygo = null;
+        }
 
         this._remoteAccess = new imports.ui.status.remoteAccess.RemoteAccessApplet();
         this._power = new imports.ui.status.power.Indicator();
@@ -795,6 +800,9 @@ class AggregateMenu extends PanelMenu.Button {
             this._indicators.add_child(this._bluetooth.indicators);
         }
         this._indicators.add_child(this._remoteAccess.indicators);
+        if (this._paygo) {
+            this._indicators.add_child(this._paygo.indicators);
+        }
         this._indicators.add_child(this._rfkill.indicators);
         this._indicators.add_child(this._volume.indicators);
         this._indicators.add_child(this._power.indicators);
@@ -809,6 +817,9 @@ class AggregateMenu extends PanelMenu.Button {
             this.menu.addMenuItem(this._bluetooth.menu);
         }
         this.menu.addMenuItem(this._remoteAccess.menu);
+        if (this._paygo) {
+            this.menu.addMenuItem(this._paygo.menu);
+        }
         this.menu.addMenuItem(this._location.menu);
         this.menu.addMenuItem(this._orientation.menu);
         this.menu.addMenuItem(this._rfkill.menu);
