ab2b34232aae72dc82f65cc0ab47c874133aeb11
Mazen Asef <mazen.asefali@gmail.com>
2019-02-20 17:19:23 +0200

    config: Remove obsolete notifications
    Pay-As-You-Go Notifications keep appearing after being
    useful when using eos-payg-ctl in the middle of a
    time-session or when the session expires normally. So we
    clean all notifications on the message tray when a
    expiry-time-changed signal is sent.
    
    https://phabricator.endlessm.com/T25246

--- js/ui/payg.js
+++ js/ui/payg.js
@@ -472,8 +472,8 @@ var PaygNotifier = new Lang.Class({
             return;
         }
 
-        if (this._notification)
-            this._notification.destroy();
+        // Clear previous notification
+        this.clearNotification();
 
         let source = new MessageTray.SystemNotificationSource();
         Main.messageTray.add(source);
@@ -511,5 +511,11 @@ var PaygNotifier = new Lang.Class({
         this._notification.connect('destroy', () => {
             this._notification = null;
         });
+
+    },
+
+    clearNotification() {
+        if (this._notification)
+            this._notification.destroy();
     },
 });
