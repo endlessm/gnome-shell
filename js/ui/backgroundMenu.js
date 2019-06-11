endlessm/gnome-shell

commit 5a939b79bb5a01e1880f5f354a75e27811494892

backgroundMenu: Adapt the background menu to Endless-specific use cases
Replace the "Display Settings" and "Settings" items with links to
"Add App", "Add Website" and "Add Folder" instead.

js/ui/backgroundMenu.js
@@ -2,6 +2,7 @@
 
 const { Clutter, St } = imports.gi;
 
+const AppActivation = imports.ui.appActivation;
 const BoxPointer = imports.ui.boxpointer;
 const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
@@ -12,8 +13,20 @@ var BackgroundMenu = class BackgroundMenu extends PopupMenu.PopupMenu {
 
         this.addSettingsAction(_("Change Background…"), 'gnome-background-panel.desktop');
         this.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        this.addSettingsAction(_("Display Settings"), 'gnome-display-panel.desktop');
-        this.addSettingsAction(_("Settings"), 'gnome-control-center.desktop');
+
+        this.addAction(_("Add App"), () => {
+            let app = Shell.AppSystem.get_default().lookup_app('org.gnome.Software.desktop');
+            let activationContext = new AppActivation.AppActivationContext(app);
+            activationContext.activate(Clutter.get_current_event());
+        });
+
+        this.addAction(_("Add Website"), () => {
+            Main.appStore.showPage(global.get_current_time(), 'web');
+        });
+
+        this.addAction(_("Add Folder"), () => {
+            Main.appStore.showPage(global.get_current_time(), 'folders');
+        });
 
         this.actor.add_style_class_name('background-menu');
 

commit 4e29b226828e39589c01230b8854fbd9b6f011c3

fixup! backgroundMenu: Adapt the background menu to Endless-specific use cases
Fallout from 5a939b79bb.

https://phabricator.endlessm.com/T26952

js/ui/backgroundMenu.js
@@ -1,6 +1,6 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 
-const { Clutter, Meta, St } = imports.gi;
+const { Clutter, Meta, Shell, St } = imports.gi;
 
 const AppActivation = imports.ui.appActivation;
 const BoxPointer = imports.ui.boxpointer;