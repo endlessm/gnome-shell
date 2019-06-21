22ef64e4d5f17f0c1a0ac055ae86944250df7e88
Umang Jain <umang@endlessm.com>
2019-06-21 13:08:03 +0530

    shellEntry: Update shell entry if browsers are disabled
    Update the shell's search-entry hint text if the default
    internet provider is enabled or disabled via parental controls.
    
    https://phabricator.endlessm.com/T26991

--- js/ui/shellEntry.js
+++ js/ui/shellEntry.js
@@ -4,10 +4,12 @@ const { Clutter, Gio, GObject, Gtk, Shell, St } = imports.gi;
 
 const Animation = imports.ui.animation;
 const BoxPointer = imports.ui.boxpointer;
+const InternetSearch = imports.ui.internetSearch;
 const Main = imports.ui.main;
 const Panel = imports.ui.panel;
 const Params = imports.misc.params;
 const PopupMenu = imports.ui.popupMenu;
+const ParentalControlsManager = imports.misc.parentalControlsManager;
 
 const Tweener = imports.ui.tweener;
 const Util = imports.misc.util;
@@ -145,16 +147,12 @@ var OverviewEntry = GObject.registerClass({
         this._spinnerAnimation = new Animation.AnimatedIcon(iconFile, SPINNER_ICON_SIZE);
         this._spinnerAnimation.actor.hide();
 
-        // Set the search entry's text based on the current search engine
-        let entryText;
-        let searchEngine = Util.getSearchEngineName();
+        this._parentalControlsManager = ParentalControlsManager.getDefault();
+        this._parentalControlsManager.connect('changed', () => {
+            this._updateSearchEntryText();
+        });
 
-        if (searchEngine != null)
-            entryText = _("Search %s and more…").format(searchEngine);
-        else
-            entryText = _("Search the internet and more…");
-
-        let hintActor = new St.Label({ text: entryText,
+        let hintActor = new St.Label({ text: '',
                                        style_class: 'search-entry-text-hint' });
 
         super._init({ name: 'searchEntry',
@@ -167,6 +165,8 @@ var OverviewEntry = GObject.registerClass({
                       secondary_icon: this._spinnerAnimation.actor,
                       x_align: Clutter.ActorAlign.CENTER,
                       y_align: Clutter.ActorAlign.CENTER });
+
+        this._updateSearchEntryText();
 
         this._blinkBrightnessEffect = new Clutter.BrightnessContrastEffect({
             enabled: false,
@@ -182,6 +182,27 @@ var OverviewEntry = GObject.registerClass({
         this.clutter_text.connect('key-press-event', this._onKeyPress.bind(this));
         this.clutter_text.connect('text-changed', this._onTextChanged.bind(this));
         global.stage.connect('notify::key-focus', this._onStageKeyFocusChanged.bind(this));
+    }
+
+    _updateSearchEntryText() {
+        let entryText;
+        let internetProvider = InternetSearch.getInternetSearchProvider();
+
+        if (internetProvider &&
+            internetProvider.appInfo &&
+            this._parentalControlsManager.shouldShowApp(internetProvider.appInfo)) {
+            // Set the search entry's text based on the current search engine
+            let searchEngine = Util.getSearchEngineName();
+
+            if (searchEngine != null)
+                entryText = _("Search %s and more…").format(searchEngine);
+            else
+                entryText = _("Search the internet and more…");
+        } else {
+            entryText = _("Search…");
+        };
+
+        this.hint_actor.set_text(entryText);
     }
 
     _isActivated() {
