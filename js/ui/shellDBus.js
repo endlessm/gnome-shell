ea5de9e967586e77d2f6bb7b7f8c0c81ce475fa3
Fabian Orccon <cfoch.fabian@gmail.com>
2019-07-03 14:18:51 -0500

    ShellDBus: Add MinimizeAll method
    It differs from the OverviewActive property since in this case
    the window stack is kept after unminimizing a single window.
    
    OverviewActive behavior
        1. Open app A
        2. Open app B
        3. Set OverviewActive to False
        4. Unminimize B
    
        Result: A and B are shown
    
    MinimizeAll behavior
        1. Open app A
        2. Open app B
        3. Call MinimizeAll method
        4. Unminimize B
    
        Result: Only B is shown
    
    https://phabricator.endlessm.com/T27144

--- js/ui/shellDBus.js
+++ js/ui/shellDBus.js
@@ -131,6 +131,12 @@ var GnomeShell = class {
         Main.overview.viewSelector.appDisplay.selectApp(id);
     }
 
+    MinimizeAll() {
+        global.get_window_actors().filter(actor => {
+            actor.metaWindow.minimize();
+        });
+    }
+
     ShowApplications() {
         Main.overview.showApps();
     }
