74323166709f146ea984ce41153201a79c35c47a
Daniel García Moreno <daniel@endlessm.com>
2019-07-10 12:21:32 +0200

    appActivation: Return value on launch invocation
    All DBUS calls to this method raises a Timeout exception because the
    launch method never returns anything to the invocation.
    
    This patch fixes that problem returning null when the application is
    activated.
    
    https://phabricator.endlessm.com/T27234

--- js/ui/shellDBus.js
+++ js/ui/shellDBus.js
@@ -634,6 +634,7 @@ var AppLauncher = class {
         }
 
         activationContext.activate(null, timestamp);
+        invocation.return_value(null);
     }
 
     LaunchViaDBusCallAsync(params, invocation) {
