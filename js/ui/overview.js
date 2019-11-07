8eef5ae271a24930f6699ce15e4548c5574d286b
Philip Chimento <philip@endlessm.com>
2019-11-06 16:52:03 -0800

    fixup! overview: Show dialog when toggling to showing windows with no apps running

--- js/ui/overview.js
+++ js/ui/overview.js
@@ -128,6 +128,9 @@ class NoWindowsDialog extends ModalDialog.ModalDialog {
             this.close(global.get_current_time());
             return Clutter.EVENT_PROPAGATE;
         });
+
+        this.opacity = 0;
+        this.visible = true;
     }
 
     show() {
