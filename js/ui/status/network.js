6aa1b4e15dffc77ba8afe977045fd72038b85f15
Andre Moreira Magalhaes <andre@endlessm.com>
2020-08-06 01:45:56 -0300

    network: Also cancel promisified async calls on destruction
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1392

--- js/ui/status/network.js
+++ js/ui/status/network.js
@@ -1616,6 +1616,8 @@ class Indicator extends PanelMenu.SystemIndicator {
     _init() {
         super._init();
 
+        this._cancellable = new Gio.Cancellable();
+
         this._primaryIndicator = this._addIndicator();
         this._vpnIndicator = this._addIndicator();
 
@@ -1653,7 +1655,13 @@ class Indicator extends PanelMenu.SystemIndicator {
     }
 
     async _getClient() {
-        this._client = await NM.Client.new_async(null);
+        try {
+            this._client = await NM.Client.new_async(this._cancellable);
+        } catch (e) {
+            if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.CANCELLED))
+                logError(e);
+            return;
+        }
 
         this._activeConnections = [];
         this._connections = [];
@@ -2015,10 +2023,14 @@ class Indicator extends PanelMenu.SystemIndicator {
             this._closeConnectivityCheck(path);
         } else if (result == PortalHelperResult.RECHECK) {
             try {
-                const state = await this._client.check_connectivity_async(null);
+                const state = await this._client.check_connectivity_async(
+                    this._cancellable);
                 if (state >= NM.ConnectivityState.FULL)
                     this._closeConnectivityCheck(path);
-            } catch (e) { }
+            } catch (e) {
+                if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.CANCELLED))
+                    log('Failed to check connectivity status: %s'.format(e.toString()));
+            }
         } else {
             log('Invalid result from portal helper: %s'.format(result));
         }
@@ -2052,7 +2064,6 @@ class Indicator extends PanelMenu.SystemIndicator {
         if (this._portalHelperProxy) {
             this._portalHelperProxy.AuthenticateRemote(path, '', timestamp);
         } else {
-            this._cancellable = new Gio.Cancellable();
             new PortalHelperProxy(Gio.DBus.session, 'org.gnome.Shell.PortalHelper',
                                   '/org/gnome/Shell/PortalHelper', (proxy, error) => {
                                       if (error) {
