9f732362f002a30312ca090b152837ab49afa8e2
Andre Moreira Magalhaes <andre@endlessm.com>
2020-08-18 20:07:50 -0300

    payg: Integrate panel indicator
    Original code imported from Endless 3.8 gnome-shell.
    
    https://phabricator.endlessm.com/T30647

--- js/ui/panel.js
+++ js/ui/panel.js
@@ -574,6 +574,7 @@ class AggregateMenu extends PanelMenu.Button {
             this._bluetooth = null;
 
         this._remoteAccess = new imports.ui.status.remoteAccess.RemoteAccessApplet();
+        this._payg = new imports.ui.status.payg.Indicator();
         this._power = new imports.ui.status.power.Indicator();
         this._powerProfiles = new imports.ui.status.powerProfiles.Indicator();
         this._rfkill = new imports.ui.status.rfkill.Indicator();
@@ -593,6 +594,8 @@ class AggregateMenu extends PanelMenu.Button {
             this._indicators.add_child(this._network);
         if (this._bluetooth)
             this._indicators.add_child(this._bluetooth);
+        if (this._payg)
+            this._indicators.add_child(this._payg);
         this._indicators.add_child(this._rfkill);
         this._indicators.add_child(this._volume);
         this._indicators.add_child(this._unsafeMode);
@@ -607,6 +610,8 @@ class AggregateMenu extends PanelMenu.Button {
 
         if (this._bluetooth)
             this.menu.addMenuItem(this._bluetooth.menu);
+        if (this._payg)
+            this.menu.addMenuItem(this._payg.menu);
 
         this.menu.addMenuItem(this._remoteAccess.menu);
         this.menu.addMenuItem(this._location.menu);
